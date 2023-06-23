e37972a11ae9e94fb106b365eceac714a719c529
Jethro_Block <jethroblock22@gmail.COM>
2023-06-23 21:20:06 +0100

    Config Axios Instance

---
+++ src/API/AxiosInstances/axiosInstance.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: process.env.NODE_ENV === 'production' 
+                ? 'https://placeholder.com' 
+                : 'http://localhost:4000',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+});
+
+export default axiosInstance;
