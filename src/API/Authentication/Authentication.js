d7cbe57f3fd8fee7bf4a69dbc403dd4daaa9b91c
Jethro_Block <jethroblock22@gmail.COM>
2023-06-23 22:16:09 +0100

    Add logIn function

--- src/API/Authentication/Authentication.js
+++ src/API/Authentication/Authentication.js
@@ -1,6 +1,7 @@
 import axiosInstance from "../AxiosInstances/axiosInstance"
 
-// POST - CREATE USER - Working!
+// POST 
+// CREATE USER - Working!
 
 
 export const createUser = async (userData = {
@@ -20,3 +21,25 @@ export const createUser = async (userData = {
   };
 
 //  console.log("CU: ", createUser()) 
+
+
+// LOG IN
+
+export const logIn = async (logInData = {
+    email: "test@example.com",
+    password: "password1"
+}) => {
+    if (!logInData) {
+      return "Please enter a valid username and password";
+    } else {
+      const response = await axiosInstance.post("/login", { user: logInData });
+      if (response.headers.authorization) {
+        window.localStorage.setItem("token", response.headers.authorization);
+        window.localStorage.setItem("userID", response.data.data.id);
+        console.log("User Logged In Successfully");
+      }
+      return response.data;
+    }
+  };
+
+  console.log("LI: ", logIn())
