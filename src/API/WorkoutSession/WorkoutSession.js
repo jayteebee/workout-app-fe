264cc9f09b3e921cac23a710f5c8c5e08173d5d6
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 18:22:41 +0100

    Add deleteWorkoutSessionByID Function

--- src/API/WorkoutSession/WorkoutSession.js
+++ src/API/WorkoutSession/WorkoutSession.js
@@ -10,7 +10,7 @@ export const getAllWorkoutSessions = async () => {
 
 // console.log(getAllWorkoutSessions());
 
-// GET WORKOUT BY ID - Working!
+// GET WORKOUT SESSION BY ID - Working!
 export const getWorkoutSessionById = async (workoutSessionID) => {
     const r = await axiosInstanceWithToken.get(`/workout_session/${workoutSessionID}`);
     return r.data;
@@ -20,7 +20,7 @@ export const getWorkoutSessionById = async (workoutSessionID) => {
 
 // POST REQUESTS
 
-// CREATE WORKOUT - Working!
+// CREATE WORKOUT SESSION - Working!
 export const createWorkoutSession = async (workoutSessionData = {user_id:5, workout_id:5}) => {
     const r = await axiosInstanceWithToken.post(`/workout_session`, workoutSessionData);
     return r.data;
@@ -31,10 +31,20 @@ export const createWorkoutSession = async (workoutSessionData = {user_id:5, work
 
 // PUT REQUESTS
 
-// EDIT WORKOUT BY ID - Working!
+// EDIT WORKOUT SESSION BY ID - Working!
 export const editWorkoutSessionByID = async (workoutSessionID, workoutSessionData = {date: "2023-06-24T17:18:33.548Z"}) => {
     const r = await axiosInstanceWithToken.put(`/workout_session/${workoutSessionID}`, workoutSessionData);
     return r.data;
 };
 
 // console.log(editWorkoutSessionByID(3));
+
+// DELETE REQUESTS
+
+// DELETE WORKOUT BY ID - working!
+export const deleteWorkoutSessionByID = async (workoutSessionID) => {
+    const r = await axiosInstanceWithToken.delete(`/workout_session/${workoutSessionID}`);
+    return r.data;
+}
+
+// console.log(deleteWorkoutSessionByID(3));
