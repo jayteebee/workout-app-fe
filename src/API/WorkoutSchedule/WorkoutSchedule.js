52a44f1444b42d6c66f5fdb2fb42884c82063e0f
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 15:29:08 +0100

    Add getWorkoutScheduleById Function

--- src/API/WorkoutSchedule/WorkoutSchedule.js
+++ src/API/WorkoutSchedule/WorkoutSchedule.js
@@ -8,4 +8,13 @@ export const getAllWorkoutSchedules = async () => {
     return r.data
 }
 
-console.log(getAllWorkoutSchedules());
+// console.log(getAllWorkoutSchedules());
+
+
+// GET WORKOUT SCHEDULE BY ID - Working!
+export const getWorkoutScheduleById = async (workoutScheduleID) => {
+    const r = await axiosInstanceWithToken.get(`/workout_schedules/${workoutScheduleID}`);
+    return r.data;
+}
+
+// console.log(getWorkoutScheduleById(20))
