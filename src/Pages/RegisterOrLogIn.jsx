31daaaf535137e1bfda272e9ea4e81dbdc7925fd
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 21:35:06 +0100

    Add Forms to LogIn/Register pages

---
+++ src/Pages/RegisterOrLogIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Register from '../Components/RegisterOrLogIn/Register'
+
+const RegisterOrLogIn = () => {
+  return (
+    <div>
+    <Register />
+    </div>
+  )
+}
+
+export default RegisterOrLogIn
