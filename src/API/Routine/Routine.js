2ba4e2b4522ff3d47e4417a919bd08afec36e66b
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 12:07:27 +0100

    Add editRoutineByID

--- src/API/Routine/Routine.js
+++ src/API/Routine/Routine.js
@@ -40,10 +40,18 @@ export const createRoutine = async (routineData = {user_id:5, name: "HIT", frequ
 
 // PUT REQUESTS
 
-// ADD WORKOUT TO ROUTINE
-export const addWorkoutToRoutine = async (routineID, workoutData = {workout_id: 1, order: 0}) => {
+// ADD WORKOUT TO ROUTINE - WORKING!
+export const addWorkoutToRoutine = async (routineID, workoutData = {workout_id: 3, order: 2}) => {
     const r = await axiosInstanceWithToken.put(`/routines/${routineID}/workouts`, workoutData);
     return r.data;
 }
 
 // console.log(addWorkoutToRoutine(4));
+
+// EDIT ROUTINE BY ID - WORKING!
+export const editRoutineByID = async (routineID, routineData = {frequency: 3}) => {
+    const r = await axiosInstanceWithToken.put(`/routines/${routineID}`, routineData);
+    return r.data;
+}
+
+// console.log(editRoutineByID(4));
