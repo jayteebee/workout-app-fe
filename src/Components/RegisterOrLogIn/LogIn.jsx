a201b67169206255a511d63860caf89563305910
Jethro_Block <jethroblock22@gmail.COM>
2023-06-24 21:37:20 +0100

    Remove null login icons

--- src/Components/RegisterOrLogIn/LogIn.jsx
+++ src/Components/RegisterOrLogIn/LogIn.jsx
@@ -32,23 +32,6 @@ const LogIn = () => {
         <p>
           Not a member? <a href='#!'>Register</a>
         </p>
-        <p>or sign up with:</p>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='facebook-f' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='google' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='twitter' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='github' />
-        </MDBBtn>
       </div>
     </form>
     </div>
