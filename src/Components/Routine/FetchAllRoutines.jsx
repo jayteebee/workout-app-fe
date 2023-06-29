358a772a9411790c38b38a67c79a8fef0ddcb4d6
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 11:27:20 +0100

    Add useEffect function for getAllRoutines API call

--- src/Components/Routine/FetchAllRoutines.jsx
+++ src/Components/Routine/FetchAllRoutines.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { getAllRoutines } from '../../API/Routine/Routine'
 
 const FetchAllRoutines = () => {
+
 const [allRoutines, setAllRoutines] = useState([])
+useEffect(() => {
+    getAllRoutines()
+    .then((data) => {
+        setAllRoutines(data);
+        console.log(allRoutines)
+    })
+    .catch((err) => {console.log("getAllRoutines API Call Failed",err)})
+},[] )
+
 
   return (
     <div>
