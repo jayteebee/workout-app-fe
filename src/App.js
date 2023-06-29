f90f39ac22daafd2fe684328cec144c37b721a34
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 09:50:43 +0100

    Remove unused component import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import RegisterOrLogIn from "./Pages/RegisterOrLogIn";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import { Routes, Route } from "react-router-dom";
-import { createUser } from "./API/Authentication/Authentication";
 import PrivateRoute from "./Utilities/PrivateRoute"
 import HomeScreen from "./Pages/HomeScreen";
 
@@ -12,13 +11,17 @@ function App() {
   return (
     <div className="App">
       <div className="App-inner">
+
       <Routes>
+
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<HomeScreen />} />
         </Route>
 
          <Route path="/GettingStarted" element={<RegisterOrLogIn />} />
-      </Routes>  
+      
+         </Routes>  
+
       </div>
     </div>
   );
