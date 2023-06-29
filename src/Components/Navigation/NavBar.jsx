7f42197b2660e2a8e6b494712b2c362bf899594c
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 09:56:34 +0100

    Create HamburgerMenu Navbar structure

---
+++ src/Components/Navigation/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {HamburgerMenu} from "../../StyleResources/HamburgerMenu.svg"
+
+const NavBar = () => {
+  return (
+    <div>
+    <img src={HamburgerMenu} alt="Menu"> </img>
+    </div>
+  )
+}
+
+export default NavBar
