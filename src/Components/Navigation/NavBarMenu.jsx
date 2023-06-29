1f970f64247dedd956ebd2ffb8778059f9894b9f
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 10:58:52 +0100

    Create Pages + Add Links

--- src/Components/Navigation/NavBarMenu.jsx
+++ src/Components/Navigation/NavBarMenu.jsx
@@ -5,13 +5,26 @@ import { Link } from 'react-router-dom'
 const NavBarMenu = ({setShowMenu}) => {
   return (
     <div className='hamburgerMenu'>
+
 <Link to="/CreateRoutine" onClick={() => setShowMenu((prevState) => !prevState)}>
     <h2>Create Routine</h2>
 </Link>
+<Link to="/CreateWorkout" onClick={() => setShowMenu((prevState) => !prevState)}>
+
     <h2>Create Workout</h2>
+</Link>
+<Link to="/Knowledge" onClick={() => setShowMenu((prevState) => !prevState)}>
+
     <h2>Knowledge</h2>
+</Link>
+<Link to="/PersonalBests" onClick={() => setShowMenu((prevState) => !prevState)}>
+
     <h2>Personal Bests</h2>
+   </Link>
+   <Link to="/Logs" onClick={() => setShowMenu((prevState) => !prevState)}>
+ 
     <h2>Logs</h2>
+  </Link>  
     </div>
   )
 }
---
+++ src/Pages/PersonalBests.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PersonalBests = () => {
+  return (
+    <div>PersonalBests</div>
+  )
+}
+
+export default PersonalBests
