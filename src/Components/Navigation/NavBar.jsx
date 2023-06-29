c0fb7928b989c05648eaa2a82f17a62c183812d3
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 10:22:35 +0100

    set menu toggle

--- src/Components/Navigation/NavBar.jsx
+++ src/Components/Navigation/NavBar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 import HamburgerMenu from '../../StyleResources/HamburgerMenu.png'
+import NavBarMenu from './NavBarMenu'
 
 const NavBar = () => {
+const [showMenu, setShowMenu] = useState(false)
+
+const toggleMenu = () => {
+    setShowMenu((prevState) => !prevState);
+}
+
   return (
     <div>
-    <img src={HamburgerMenu} alt="Menu" /> 
+    <img src={HamburgerMenu} alt="Menu" onClick={toggleMenu} /> 
+    {showMenu ? <NavBarMenu /> : null}
     </div>
   )
 }
---
+++ src/Components/Navigation/NavBarMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBarMenu = () => {
+  return (
+    <div>NavBarMenu</div>
+  )
+}
+
+export default NavBarMenu
