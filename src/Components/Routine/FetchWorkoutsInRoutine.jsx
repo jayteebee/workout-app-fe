b7ddb7751869c926decbee6c25224fb321d714b0
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 13:43:52 +0100

    fetchWorkoutsInRoutine function config Works on button press, not yet connected to workout button

---
+++ src/Components/Routine/FetchWorkoutsInRoutine.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { getWorkoutsInRoutine } from '../../API/Routine/Routine'
+
+const FetchWorkoutsInRoutine = () => {
+    const [workoutsInRoutine, setWorkoutsInRoutine] = useState([])
+
+    const fetchWorkoutsInRoutine = async (rID) => {
+        getWorkoutsInRoutine(rID)
+        .then((data) => {
+            setWorkoutsInRoutine(data)
+            console.log(data)
+        })
+        .catch((err) => console.log("getWorkoutsInRoutine API Call Failed",err))
+    }
+  return (
+    <div>
+    <button onClick={() => fetchWorkoutsInRoutine(3)}></button>
+
+    </div>
+  )
+}
+
+export default FetchWorkoutsInRoutine
--- src/Pages/Routines.jsx
+++ src/Pages/Routines.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import "../CSS/Routines.css"
-import FetchAllRoutines from '../Components/Routine/FetchAllRoutines'
-import FetchRoutineByID from '../Components/Routine/FetchRoutineByID'
+import React from "react";
+import "../CSS/Routines.css";
+import FetchAllRoutines from "../Components/Routine/FetchAllRoutines";
+import FetchRoutineByID from "../Components/Routine/FetchRoutineByID";
+import FetchWorkoutsInRoutine from "../Components/Routine/FetchWorkoutsInRoutine";
 
 const Routines = () => {
   return (
     <div>
-   <h3>Routines</h3> 
-   <div className='fetchAllRoutines'>
-   <FetchAllRoutines />
-   </div>
-   <div>
-   <FetchRoutineByID />
-   </div>
+      <h3>Routines</h3>
+      <div className="fetchAllRoutines">
+        <FetchAllRoutines />
+      </div>
+      <div className="fetchRoutineByID">
+        <FetchRoutineByID />
+      </div>
+      <div>
+        <FetchWorkoutsInRoutine />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Routines
+export default Routines;
