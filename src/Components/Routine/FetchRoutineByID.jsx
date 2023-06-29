36e70ecffdddf37eda75b031584979789aa5c921
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 12:20:05 +0100

    Add fetchRoutineByID function

--- src/Components/Routine/FetchRoutineByID.jsx
+++ src/Components/Routine/FetchRoutineByID.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getRoutineById } from '../../API/Routine/Routine'
 
 const FetchRoutineByID = () => {
-    
+    const [routine, setRoutine] = useState({})
+
+    const fetchRoutineByID = async (rID) => {
+        getRoutineById(rID)
+        .then((data) => {
+            setRoutine(data)
+        })
+        .catch((err) => {console.log("getRoutineByID API Call Failed",err)})
+    }
+    // console.log(fetchRoutineByID(3))
   return (
     <div>
     
--- src/Pages/Routines.jsx
+++ src/Pages/Routines.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../CSS/Routines.css"
 import FetchAllRoutines from '../Components/Routine/FetchAllRoutines'
+import FetchRoutineByID from '../Components/Routine/FetchRoutineByID'
 
 const Routines = () => {
   return (
@@ -8,6 +9,9 @@ const Routines = () => {
    <h3>Routines</h3> 
    <div className='fetchAllRoutines'>
    <FetchAllRoutines />
+   </div>
+   <div>
+   <FetchRoutineByID />
    </div>
     </div>
   )
