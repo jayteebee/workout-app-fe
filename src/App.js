01e00c07666549393d53f16a7cf107748fc394cf
Jethro_Block <jethroblock22@gmail.COM>
2023-06-29 11:06:39 +0100

    Reformat NavBar links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import RegisterOrLogIn from "./Pages/RegisterOrLogIn";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import { Routes, Route } from "react-router-dom";
-import PrivateRoute from "./Utilities/PrivateRoute"
+import PrivateRoute from "./Utilities/PrivateRoute";
 import HomeScreen from "./Pages/HomeScreen";
 import NavBar from "./Components/Navigation/NavBar";
 import CreateRoutine from "./Pages/CreateRoutine";
@@ -11,37 +11,30 @@ import Knowledge from "./Pages/Knowledge";
 import PersonalBests from "./Pages/PersonalBests";
 import Logs from "./Pages/Logs";
 
-
 // brew services start redis - backend service
 
 function App() {
   return (
     <div className="App">
       <div className="App-inner">
-<NavBar />
-      <Routes>
+        <NavBar />
+        <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<HomeScreen />} />
 
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<HomeScreen />} />
+            <Route path="/CreateRoutine" element={<CreateRoutine />} />
 
-          <Route path="/CreateRoutine" element={<CreateRoutine />} />
-          
-          <Route path="/CreateWorkout" element={<CreateWorkout />} />
+            <Route path="/CreateWorkout" element={<CreateWorkout />} />
 
-          <Route path="/Knowledge" element={<Knowledge />} />
+            <Route path="/Knowledge" element={<Knowledge />} />
 
-          <Route path="/PersonalBests" element={<PersonalBests />} />
+            <Route path="/PersonalBests" element={<PersonalBests />} />
 
-          <Route path="/Logs" element={<Logs />} />
+            <Route path="/Logs" element={<Logs />} />
+          </Route>
 
-        </Route>
-
-        
-
-         <Route path="/GettingStarted" element={<RegisterOrLogIn />} />
-      
-         </Routes>  
-
+          <Route path="/GettingStarted" element={<RegisterOrLogIn />} />
+        </Routes>
       </div>
     </div>
   );
--- src/Components/Navigation/NavBarMenu.jsx
+++ src/Components/Navigation/NavBarMenu.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import "../../CSS/NavBar.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../../CSS/NavBar.css";
+import { Link } from "react-router-dom";
 
-const NavBarMenu = ({setShowMenu}) => {
+const NavBarMenu = ({ setShowMenu }) => {
+  const toggleMenu = () => {
+    setShowMenu((prevState) => !prevState);
+  };
+
   return (
-    <div className='hamburgerMenu'>
-
-<Link to="/CreateRoutine" onClick={() => setShowMenu((prevState) => !prevState)}>
-    <h2>Create Routine</h2>
-</Link>
-<Link to="/CreateWorkout" onClick={() => setShowMenu((prevState) => !prevState)}>
-
-    <h2>Create Workout</h2>
-</Link>
-<Link to="/Knowledge" onClick={() => setShowMenu((prevState) => !prevState)}>
-
-    <h2>Knowledge</h2>
-</Link>
-<Link to="/PersonalBests" onClick={() => setShowMenu((prevState) => !prevState)}>
-
-    <h2>Personal Bests</h2>
-   </Link>
-   <Link to="/Logs" onClick={() => setShowMenu((prevState) => !prevState)}>
- 
-    <h2>Logs</h2>
-  </Link>  
+    <div className="hamburgerMenu">
+      <Link to="/CreateRoutine" onClick={toggleMenu}>
+        <h2>Create Routine</h2>
+      </Link>
+      <Link to="/CreateWorkout" onClick={toggleMenu}>
+        <h2>Create Workout</h2>
+      </Link>
+      <Link to="/Knowledge" onClick={toggleMenu}>
+        <h2>Knowledge</h2>
+      </Link>
+      <Link to="/PersonalBests" onClick={toggleMenu}>
+        <h2>Personal Bests</h2>
+      </Link>
+      <Link to="/Logs" onClick={toggleMenu}>
+        <h2>Logs</h2>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default NavBarMenu
+export default NavBarMenu;
