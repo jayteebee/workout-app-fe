68c190991e3f779af5eeb0899fd75d35038a793a
Jethro_Block <jethroblock22@gmail.COM>
2023-06-30 15:39:14 +0100

    DeleteWorkout config

--- src/Components/Workout/DeleteWorkout.jsx
+++ src/Components/Workout/DeleteWorkout.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { deleteWorkoutByID } from '../../API/Workout/Workout'
 
-const DeleteWorkout = () => {
+const DeleteWorkout = ({workoutToDelete, setWorkoutToDelete, setWorkoutToggle }) => {
+
+    useEffect(() => {
+        deleteWorkoutByID(workoutToDelete)
+        .then(() => {setWorkoutToDelete(null)
+        })
+        .catch((err) => console.error(err))
+        setWorkoutToggle((prevState) => !prevState)
+    }, [workoutToDelete, setWorkoutToDelete ])
+
   return (
-    <div>DeleteWorkout</div>
+    <div></div>
   )
 }
 
