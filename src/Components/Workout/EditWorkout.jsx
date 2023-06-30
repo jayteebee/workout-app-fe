35bf61216c5fa1d9f96293e828e529a6ddcad074
Jethro_Block <jethroblock22@gmail.COM>
2023-06-30 15:34:37 +0100

    Big Fixed - cant edit two names in a row

--- src/Components/Workout/EditWorkout.jsx
+++ src/Components/Workout/EditWorkout.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { parseJwt } from '../../API/Authentication/parseJwt';
+import {
+    MDBInput,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
+import { editWorkoutByID } from '../../API/Workout/Workout';
 
-const EditWorkout = () => {
+const EditWorkout = ({workoutToEdit, editToggle, setEditToggle}) => {
+    const [formInput, setFormInput] = useState({user_id: "", name:""})
+
+    useEffect(() => {
+        const token = window.localStorage.getItem("token");
+        const decodedToken = parseJwt(token);
+        const userID = decodedToken.sub;
+        setFormInput(prevState => ({...prevState, user_id: userID}));
+    },[editToggle])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          await editWorkoutByID(workoutToEdit,formInput);
+        } catch (err) {
+          console.error("Error:",err);
+        } finally {
+          setFormInput({user_id: "", name:""});
+          setEditToggle((prevState) => !prevState)
+        }
+      }
+        
+      const handlechange = (e) => {
+        setFormInput({
+          ...formInput,
+          [e.target.name]: e.target.value,
+        })
+      };
+
   return (
-    <div>EditWorkout</div>
+    <div>
+    <form onSubmit={handleSubmit}>
+    <MDBInput className='mb-4' type='text' label='Edit Routine Name' value={formInput.name} name="name" onChange={handlechange} />
+    <MDBBtn type='submit' className='mb-4' block>
+      Edit
+    </MDBBtn>
+
+  </form>
+    
+    </div>
   )
 }
 
--- src/Components/Workout/FetchAllWorkouts.jsx
+++ src/Components/Workout/FetchAllWorkouts.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { getAllWorkouts } from '../../API/Workout/Workout'
+import EditWorkout from './EditWorkout'
 import FetchExercisesInWorkout from './FetchExercisesInWorkout'
 
 const FetchAllWorkouts = ({workoutToggle}) => {
     const [allWorkouts, setAllWorkouts] = useState([])
     const [selectedExerciseID, setSelectedExerciseID] = useState(null)
+    const [workoutToEdit, setWorkoutToEdit] = useState(null)
+    const [editToggle, setEditToggle] = useState(false)
 
     useEffect(() => {
         getAllWorkouts()
@@ -12,18 +15,20 @@ const FetchAllWorkouts = ({workoutToggle}) => {
             setAllWorkouts(data);
         })
         .catch((err) => {console.log("getAllWorkouts API Call Failed",err)})
-    },[workoutToggle] )
+    },[workoutToggle, editToggle] )
 
   return (
     <div>
     {allWorkouts.map((workout) => (
         <div key={workout.id}> 
         <button onClick={() => setSelectedExerciseID(workout.id)}>{workout.name}</button>
-        
+        <button onClick={() => setWorkoutToEdit(workout.id)}>Change Name</button>
+
         </div>
     ))}
 {selectedExerciseID && <FetchExercisesInWorkout selectedExerciseID={selectedExerciseID} />}
-    
+{workoutToEdit && <EditWorkout workoutToEdit={workoutToEdit} editToggle={editToggle} setEditToggle={setEditToggle}/>}
+   
     </div>
   )
 }
