79267f279fcb4f10a60257cedffc6ec841b27f3f
Jethro_Block <jethroblock22@gmail.COM>
2023-06-30 14:57:40 +0100

    CreateWorkout config

--- src/Components/Workout/CreateWorkout.jsx
+++ src/Components/Workout/CreateWorkout.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    MDBInput,
+    MDBCol,
+    MDBRow,
+    MDBCheckbox,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
+import { createWorkout } from '../../API/Workout/Workout';
+
 
 const CreateWorkout = () => {
+    const [formInput, setFormInput] = useState({name:"", frequency:"", user_id: ""})
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          await createWorkout(formInput);
+        } catch (err) {
+          console.error("Error:",err);
+        } finally {
+          setFormInput({});
+          setRoutineToggle((prevState) => !prevState);
+        }
+      }
+        
+      const handlechange = (e) => {
+        setFormInput({
+          ...formInput,
+          [e.target.name]: e.target.value,
+        })
+      };
+
   return (
-    <div>CreateWorkout</div>
+    <div>
+    
+    <form onSubmit={handleSubmit}>
+    <MDBInput className='mb-4' type='text' label='Workout Name' value={formInput.name} name="name" onChange={handlechange} />
+
+    <MDBBtn type='submit' className='mb-4' block>
+      Create Routine
+    </MDBBtn>
+
+  </form>
+    
+    </div>
   )
 }
 
