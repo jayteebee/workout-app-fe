4725ff4ff5ab5526313033b0dea68cff17c22af5
Jethro_Block <jethroblock22@gmail.COM>
2023-06-30 10:25:05 +0100

    Add Delete Button and state config

--- src/Components/Routine/FetchAllRoutines.jsx
+++ src/Components/Routine/FetchAllRoutines.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { getAllRoutines } from '../../API/Routine/Routine'
+import DeleteRoutine from './DeleteRoutine'
 import EditRoutine from './EditRoutine'
 import FetchWorkoutsInRoutine from './FetchWorkoutsInRoutine'
 
@@ -10,6 +11,8 @@ const [allRoutines, setAllRoutines] = useState([])
 const [selectedRoutineID, setSelectedRoutineID] = useState(null)
 const [routineToEdit, setRoutineToEdit] = useState(null)
 const [editToggle, setEditToggle] = useState(false)
+const [routineToDelete, setRoutineToDelete] = useState(null)
+
 useEffect(() => {
     getAllRoutines()
     .then((data) => {
@@ -25,10 +28,13 @@ useEffect(() => {
         <button onClick={() => setSelectedRoutineID(routine.id)}>{routine.name}</button> 
         {routine.frequency}
         <button onClick={() => setRoutineToEdit(routine.id)}>Change Name/Frequency</button>
+        <button onClick={() => setRoutineToDelete(routine.id)}></button>
         </div>
     ))}
     {selectedRoutineID && <FetchWorkoutsInRoutine rID={selectedRoutineID} />}
     {routineToEdit && <EditRoutine eID={routineToEdit} setEditToggle={setEditToggle}/>}
+    {routineToDelete && <DeleteRoutine routineToDelete={routineToDelete} />}
+    
     </div>
   )
 }
