1c49ff06069f5c8fd81b81554086a54df47d2a73
Jethro_Block <jethroblock22@gmail.COM>
2023-06-30 14:10:33 +0100

    Add CreateWorkout features

--- src/Pages/CreateWorkout.jsx
+++ src/Pages/CreateWorkout.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import FetchAllWorkouts from '../Components/Workout/FetchAllWorkouts'
 
 const CreateWorkout = () => {
   return (
-    <div>CreateWorkout</div>
+    <div>
+    <h3>CreateWorkout</h3>
+    <div className='fetchAllWorkouts'>
+    <FetchAllWorkouts />
+    </div>
+
+    </div>
   )
 }
 
