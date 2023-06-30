a9b81cbcb0430e7eb8e9247308e7f750901da67c
Jethro_Block <jethroblock22@gmail.COM>
2023-06-30 15:19:49 +0100

    Formatting

--- src/Components/Routine/EditRoutine.jsx
+++ src/Components/Routine/EditRoutine.jsx
@@ -1,57 +1,73 @@
-import React, {useState, useEffect} from 'react'
-import { parseJwt } from '../../API/Authentication/parseJwt';
+import React, { useState, useEffect } from "react";
+import { parseJwt } from "../../API/Authentication/parseJwt";
 import {
-    MDBInput,
-    MDBCol,
-    MDBRow,
-    MDBCheckbox,
-    MDBBtn
-  } from 'mdb-react-ui-kit';
-import { editRoutineByID } from '../../API/Routine/Routine';
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import { editRoutineByID } from "../../API/Routine/Routine";
 
-const EditRoutine = ({eID, setEditToggle}) => {
-    const [formInput, setFormInput] = useState({name:"", frequency:"", user_id: ""})
-    console.log(formInput)
+const EditRoutine = ({ eID, setEditToggle }) => {
+  const [formInput, setFormInput] = useState({
+    name: "",
+    frequency: "",
+    user_id: "",
+  });
+  console.log(formInput);
 
   useEffect(() => {
-      const token = window.localStorage.getItem("token");
-      const decodedToken = parseJwt(token);
-      const userID = decodedToken.sub;
-      setFormInput(prevState => ({...prevState, user_id: userID}));
-  },[])
-  
-      const handleSubmit = async (e) => {
-          e.preventDefault();
-          try {
-            await editRoutineByID(eID,formInput);
-          } catch (err) {
-            console.error("Error:",err);
-          } finally {
-            setFormInput({});
-            setEditToggle((prevState) => !prevState)
-          }
-        }
-          
-        const handlechange = (e) => {
-          setFormInput({
-            ...formInput,
-            [e.target.name]: e.target.value,
-          })
-        };
+    const token = window.localStorage.getItem("token");
+    const decodedToken = parseJwt(token);
+    const userID = decodedToken.sub;
+    setFormInput((prevState) => ({ ...prevState, user_id: userID }));
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await editRoutineByID(eID, formInput);
+    } catch (err) {
+      console.error("Error:", err);
+    } finally {
+      setFormInput({});
+      setEditToggle((prevState) => !prevState);
+    }
+  };
+
+  const handlechange = (e) => {
+    setFormInput({
+      ...formInput,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div>
-    <form onSubmit={handleSubmit}>
-    <MDBInput className='mb-4' type='text' label='Edit Routine Name' value={formInput.name} name="name" onChange={handlechange} />
-    <MDBInput className='mb-4' type='text' label='Edit Frequency' value={formInput.frequency} name="name" onChange={handlechange} />
-    <MDBBtn type='submit' className='mb-4' block>
-      Edit
-    </MDBBtn>
-
-  </form>
-    
+      <form onSubmit={handleSubmit}>
+        <MDBInput
+          className="mb-4"
+          type="text"
+          label="Edit Routine Name"
+          value={formInput.name}
+          name="name"
+          onChange={handlechange}
+        />
+        <MDBInput
+          className="mb-4"
+          type="text"
+          label="Edit Frequency"
+          value={formInput.frequency}
+          name="name"
+          onChange={handlechange}
+        />
+        <MDBBtn type="submit" className="mb-4" block>
+          Edit
+        </MDBBtn>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default EditRoutine
+export default EditRoutine;
