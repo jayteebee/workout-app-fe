9966ffbbb291215bebdedae46895feb69e098a5b
Jethro_Block <jethroblock22@gmail.COM>
2023-08-03 15:37:29 +0100

    Add Input bar, add state

--- src/Components/Exercises/Search.jsx
+++ src/Components/Exercises/Search.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MDBInputGroup, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 
 
 const Search = () => {
+
+const [formInput, setFormInput] = useState({exercise: ""})
+console.log("FormInput - Search",formInput)
+
+const handleChange = (e) => {
+    setFormInput({
+        ...formInput,
+        [e.target.name]: e.target.value,
+      });
+}
+
   return (
     <div>
     <h3>Search</h3>
-    
+
     <MDBInputGroup>
-    <MDBInput label='Search' />
+    <MDBInput 
+    label='Search'
+    value={formInput.exercise}
+    name="exercise"
+    onChange={handleChange}
+    />
     <MDBBtn rippleColor='dark'> Search </MDBBtn>
   </MDBInputGroup>
     
