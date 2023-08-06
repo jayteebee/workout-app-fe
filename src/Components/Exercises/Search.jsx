a83e35eab8f5d63e8bc049b3c0379b9d4c9a7611
Jethro_Block <jethroblock22@gmail.COM>
2023-08-06 12:11:37 +0100

    Add exercises to dropdown

--- src/Components/Exercises/Search.jsx
+++ src/Components/Exercises/Search.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { MDBInputGroup, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
-
+import { MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem } from 'mdb-react-ui-kit';
+import { getAllExercises } from '../../API/Exercise/Exercise'
 
 const Search = () => {
 
 const [formInput, setFormInput] = useState({exercise: ""})
 console.log("FormInput - Search",formInput)
+
+const [allExercises, setAllExercises] = useState([])
+
+useEffect(() => {
+  getAllExercises()
+  .then((data) => {
+      setAllExercises(data);
+  })
+  .catch((err) => {console.log("getAllExercises API Call Failed: ",err)})
+}, [])
+console.log(allExercises)
+
 
 const handleChange = (e) => {
     setFormInput({
@@ -28,7 +41,20 @@ const handleChange = (e) => {
     <MDBBtn rippleColor='dark'> Search </MDBBtn>
   </MDBInputGroup>
     
+  <MDBDropdown>
+  <MDBDropdownToggle>Dropdown button</MDBDropdownToggle>
+  <MDBDropdownMenu>
+{allExercises.map((exercise, index) => (
+  <div key={index}>
+    <MDBDropdownItem link>{exercise.name}</MDBDropdownItem>
+  </div>
+))}
+
     
+
+  </MDBDropdownMenu>
+</MDBDropdown>
+
     </div>
   )
 }
