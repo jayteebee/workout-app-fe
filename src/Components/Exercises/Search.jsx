7129703fe77e8591f70ed5af3fae5c50954038ee
Jethro_Block <jethroblock22@gmail.COM>
2023-08-06 14:50:31 +0100

    Add Feature: Search + Filter by exercise

--- src/Components/Exercises/Search.jsx
+++ src/Components/Exercises/Search.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { MDBInputGroup, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
-import { MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem } from 'mdb-react-ui-kit';
 import { getAllExercises } from '../../API/Exercise/Exercise'
+import ReactSelect from 'react-select';
 
 const Search = () => {
 
-const [formInput, setFormInput] = useState({exercise: ""})
+const [formInput, setFormInput] = useState(null)
 console.log("FormInput - Search",formInput)
 
 const [allExercises, setAllExercises] = useState([])
@@ -19,44 +18,57 @@ useEffect(() => {
 }, [])
 console.log(allExercises)
 
+const exercises = allExercises.map((exercise) => ({
+  label: exercise.name
+}))
 
-const handleChange = (e) => {
-    setFormInput({
-        ...formInput,
-        [e.target.name]: e.target.value,
-      });
+const handleChange = (formInput) => {
+    setFormInput(formInput);
 }
 
   return (
     <div>
     <h3>Search</h3>
 
-    <MDBInputGroup>
-    <MDBInput 
-    label='Search'
-    value={formInput.exercise}
-    name="exercise"
-    onChange={handleChange}
-    />
-    <MDBBtn rippleColor='dark'> Search </MDBBtn>
-  </MDBInputGroup>
-    
-  <MDBDropdown>
-  <MDBDropdownToggle>Dropdown button</MDBDropdownToggle>
-  <MDBDropdownMenu>
-{allExercises.map((exercise, index) => (
-  <div key={index}>
-    <MDBDropdownItem link>{exercise.name}</MDBDropdownItem>
-  </div>
-))}
-
-    
-
-  </MDBDropdownMenu>
-</MDBDropdown>
+<ReactSelect 
+placeholder="Search By Exercise Name"
+options={exercises}
+  value={formInput}
+  onChange={handleChange}
+/>
 
     </div>
   )
 }
 
 export default Search
+
+
+// <MDBInputGroup>
+// <MDBInput 
+// label='Search Exercises By Name...'
+// value={formInput.exercise}
+// name="exercise"
+// onChange={handleChange}
+// />
+
+// <MDBDropdown>
+// <MDBDropdownToggle>Dropdown button</MDBDropdownToggle>
+// <MDBDropdownMenu>
+// {allExercises.map((exercise, index) => (
+// <div key={index}>
+// <MDBDropdownItem link>{exercise.name}</MDBDropdownItem>
+// </div>
+// ))}
+
+// </MDBDropdownMenu>
+// </MDBDropdown>
+// </MDBInputGroup>
+
+
+// const handleChange = (e) => {
+//   setFormInput({
+//       ...formInput,
+//       [e.target.name]: e.target.value,
+//     });
+// }
