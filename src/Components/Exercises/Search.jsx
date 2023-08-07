9a26eb6490336df97e3e8448e3fd6b3d91cb55c1
Jethro_Block <jethroblock22@gmail.COM>
2023-08-07 14:59:01 +0100

    Conditional working for exercises filter

--- src/Components/Exercises/Search.jsx
+++ src/Components/Exercises/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getAllExercises } from "../../API/Exercise/Exercise";
 import ReactSelect from "react-select";
+import { MDBBtn } from "mdb-react-ui-kit";
 
 const Search = () => {
   const [searchedExerciseName, setSearchedExerciseName] = useState(null);
@@ -8,6 +9,24 @@ const Search = () => {
   const [searchedMuscleGroup, setSearchedMuscleGroup] = useState(null);
   console.log("searchedMuscleGroup - Search", searchedMuscleGroup);
   const [allExercises, setAllExercises] = useState([]);
+  const [barbells, setBarbells] = useState(false);
+  const [dumbbells, setDumbbells] = useState(false);
+  const [machines, setMachines] = useState(false);
+
+  const barbellToggle = () => {
+    setBarbells((prevState) => !prevState);
+  };
+  console.log("Barbells: ", barbells);
+
+  const dumbbellToggle = () => {
+    setDumbbells((prevState) => !prevState);
+  };
+  console.log("Dumbbells: ", dumbbells);
+
+  const machineToggle = () => {
+    setMachines((prevState) => !prevState);
+  };
+  console.log("Machines: ", machines);
 
   useEffect(() => {
     getAllExercises()
@@ -18,10 +37,11 @@ const Search = () => {
         console.log("getAllExercises API Call Failed: ", err);
       });
   }, []);
-  console.log(allExercises);
+  // console.log(allExercises);
 
   const exerciseName = allExercises.map((exercise) => ({
     label: exercise.name,
+    value: exercise.name,
   }));
 
   const handleChangeForExerciseName = (searchedExerciseName) => {
@@ -34,15 +54,67 @@ const Search = () => {
 
   const muscleGroup = allExercises.map((exercise) => ({
     label: exercise.name,
-    value: exercise.primary_muscles
+    value: exercise.name,
   }));
 
+  const barbellFilter = allExercises.filter(
+    (exercise) => exercise.equipment_used === "Barbell"
+  );
+  const barbellFilterMapped = barbellFilter.map((exercise) => ({
+    label: exercise.name,
+    value: exercise.name,
+  }));
+  // console.log("barbellFilterMapped", barbellFilterMapped)
+
+  const dumbbellFilter = allExercises.filter(
+    (exercise) => exercise.equipment_used === "Dumbbells"
+  );
+  const dumbbellFilterMapped = dumbbellFilter.map((exercise) => ({
+    label: exercise.name,
+    value: exercise.name,
+  }));
+  // console.log("dumbbellFilterMapped", dumbbellFilterMapped)
+
+  const machineFilter = allExercises.filter(
+    (exercise) => exercise.equipment_used === "Machine"
+  );
+  const machineFilterMapped = machineFilter.map((exercise) => ({
+    label: exercise.name,
+    value: exercise.name,
+  }));
+  // console.log("machineFilterMapped", machineFilterMapped)
+
+let options = []
+if (barbells && dumbbells && machines) {
+  options = exerciseName
+   }
+else if (barbells && dumbbells) {
+  options = [...barbellFilterMapped, ...dumbbellFilterMapped]
+ } else if (barbells && machines) {
+  options = [...barbellFilterMapped, ...machineFilterMapped]
+ } else if (dumbbells && machines) {
+  options = [...dumbbellFilterMapped, ...machineFilterMapped]
+ } else if (barbells) {
+  options = barbellFilterMapped
+} else if (dumbbells) {
+  options = dumbbellFilterMapped
+} else if (machines) {
+  options = machineFilterMapped
+} else {
+  options = exerciseName
+}
+console.log("options", options)
   return (
     <div>
       <h3>Search</h3>
+
+      <MDBBtn onClick={barbellToggle}>Barbells</MDBBtn>
+      <MDBBtn onClick={dumbbellToggle}>Dumbbells</MDBBtn>
+      <MDBBtn onClick={machineToggle}>Machines</MDBBtn>
+
       <ReactSelect
         placeholder="Search By Exercise Name"
-        options={exerciseName}
+        options={options}
         value={searchedExerciseName}
         onChange={handleChangeForExerciseName}
       />
