ae7e42020969331eb9f39a3fdc3743b936604573
Jethro_Block <jethroblock22@gmail.COM>
2023-08-08 18:53:28 +0100

    remove comments

--- src/Components/Exercises/SetsRepsWeight.jsx
+++ src/Components/Exercises/SetsRepsWeight.jsx
@@ -7,13 +7,6 @@ const SetsRepsWeight = ({ searchedExerciseName, searchedMuscleGroup, setExercise
   const [sets, setSets] = useState(null);
   const [reps, setReps] = useState(null);
   const [weight, setWeight] = useState(null);
-//   const [exerciseParameters, setExerciseParameters] = useState({
-//     name: null,
-//     sets: 0,
-//     reps: 0,
-//     weight: 0,
-//   });
-//   console.log("exerciseParameters", exerciseParameters);
 
   useEffect(() => {
     if (sets !== null || reps !== null || weight !== null) {
