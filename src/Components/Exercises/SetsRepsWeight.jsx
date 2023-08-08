0ba29df3163187e9d5bcec4daf57b8f25b8998a6
Jethro_Block <jethroblock22@gmail.COM>
2023-08-08 14:45:53 +0100

    Add Sets/Reps/Weight counters and store value

--- src/Components/Exercises/SetsRepsWeight.jsx
+++ src/Components/Exercises/SetsRepsWeight.jsx
@@ -1,16 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactSelect from "react-select";
 
-const SetsRepsWeight = ({searchedExerciseName, searchedMuscleGroup}) => {
-  console.log("searchedExerciseName:SRW", searchedExerciseName)
-  console.log("searchedMuscleGroup:SRW", searchedMuscleGroup)
+const SetsRepsWeight = ({ searchedExerciseName, searchedMuscleGroup }) => {
+  console.log("searchedExerciseName:SRW", searchedExerciseName);
+  console.log("searchedMuscleGroup:SRW", searchedMuscleGroup);
+  const [exerciseParameters, setExerciseParameters] = useState({
+    sets: 0,
+    reps: 0,
+    weight: 0,
+  });
+  console.log("exerciseParameters", exerciseParameters);
 
-  
-    return (
+  const [sets, setSets] = useState(null);
+  const [reps, setReps] = useState(null);
+  const [weight, setWeight] = useState(null);
+  console.log("sets", sets);
+  console.log("reps", reps);
+  console.log("weight", weight);
+
+
+
+  let setOptions = [];
+  for (let i = 0; i < 21; i++) {
+    setOptions.push({ label: `${i}`, value: i });
+  }
+
+  let repOptions = [];
+  for (let i = 0; i < 40; i++) {
+    repOptions.push({ label: `${i}`, value: i });
+  }
+
+  let weightOptions = [];
+  for (let i = 0; i < 500; i += 2.5) {
+    weightOptions.push({ label: `${i}`, value: i });
+  }
+console.log("weightOptions",weightOptions)
+  const updateSets = (sets) => {
+    setSets(sets);
+  };
+
+  const updateReps = (reps) => {
+    setReps(reps);
+  };
+
+  const updateWeight = (weight) => {
+    setWeight(weight);
+  };
+
+  return (
     <div>
- 
-    
+      <h3>Sets</h3>
+      <ReactSelect
+        placeholder="Sets"
+        options={setOptions}
+        value={sets}
+        onChange={updateSets}
+      />
+      <h3>Reps</h3>
+      <ReactSelect
+        placeholder="Reps"
+        options={repOptions}
+        value={reps}
+        onChange={updateReps}
+      />
+      <h3>Weight</h3>
+      <ReactSelect
+        placeholder="Weight"
+        options={weightOptions}
+        value={weight}
+        onChange={updateWeight}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SetsRepsWeight
+export default SetsRepsWeight;
