43098023ad1e8d7f5232456984e59e7c4f60b2d3
Jethro_Block <jethroblock22@gmail.COM>
2023-08-08 19:42:55 +0100

    Populate viewexercise with logic

--- src/Pages/ViewExercises.jsx
+++ src/Pages/ViewExercises.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import { getExercisesInWorkout } from '../API/Workout/Workout';
 
 const ViewExercises = () => {
+    const location = useLocation();
+    const workoutID = location.state?.workoutId
+console.log("VE WID", workoutID)
+
+const [exercises, setExercises] = useState([])
+useEffect(() => {
+    if (workoutID) {
+        getExercisesInWorkout(workoutID)
+        .then((data) => {
+            setExercises(data)
+        })
+        .catch((err) => {console.log("getExercisesInWorkout API Call Failed",err)})
+    }
+}, [workoutID])
+
+console.log("exercises", exercises)
   return (
-    <div>ViewExercises</div>
+    <div>
+    <h3>Workout</h3>
+
+{exercises.map((exercises, index) => (
+    <div key={index}>
+        <p>REPS:{exercises.reps}</p>
+        <p>NAME: {exercises.exercise.name}</p>
+
+    </div>
+  ))}
+    </div>
   )
 }
 
