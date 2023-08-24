40b396dd53ad197cd907f72cf46f3163d3f61aad
Jethro_Block <jethroblock22@gmail.COM>
2023-08-24 22:46:30 +0100

    Workout Schedule Printed to homescreen

--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getAllWorkoutSchedules } from "../API/WorkoutSchedule/WorkoutSchedule";
+import format from "date-fns/format";
 
 const HomeScreen = () => {
-  return (
-    <div className='scrollableDiv'>HomeScreen</div>
-  )
-}
+  const [workoutSchedule, setWorkoutSchedule] = useState([]);
 
-export default HomeScreen
+  useEffect(() => {
+    getAllWorkoutSchedules()
+      .then((data) => {
+        setWorkoutSchedule(data);
+      })
+      .catch((err) => {
+        console.log("getAllWorkoutSchedules API Call Failed", err);
+      });
+  }, []);
+  console.log("workoutSchedule", workoutSchedule);
+  const sortedSchedule = workoutSchedule.slice().sort((a, b) => a.id - b.id);
+  console.log("sortedSchedule", sortedSchedule);
+
+  // const schedule = workoutSchedule.map((date, index) => {
+  //   const inputDate = new Date (`${date.date}`)
+  //   const formattedDate = format(inputDate, 'EEEE do MMMM yyyy')
+  //   console.log("DC",date.completed)
+  //   return (
+  //     <div>
+  //     <p>{formattedDate}</p>
+  //     <p>{date.completed ? <p>Completed</p> : <p>Not Completed</p>}</p>
+  //     </div>
+  //     )
+  // })
+  // console.log("schedule", schedule)
+
+  return (
+    <div className="scrollableDiv">
+      {sortedSchedule.map((date, index) => {
+        const inputDate = new Date(`${date.date}`);
+        const formattedDate = format(inputDate, "EEEE do MMMM yyyy");
+        return (
+          <div key={index}>
+            <h3>{formattedDate}</h3>
+            {date.completed ? <p>Completed</p> : <p>Not Completed</p>}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HomeScreen;
