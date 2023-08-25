4bd36287ecdc5590fc0454ce4ab51eea422cfbde
Jethro_Block <jethroblock22@gmail.COM>
2023-08-25 17:24:08 +0100

    Add Calendar

--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { getAllWorkoutSchedules } from "../API/WorkoutSchedule/WorkoutSchedule";
 import format from "date-fns/format";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const HomeScreen = () => {
   const [workoutSchedule, setWorkoutSchedule] = useState([]);
+  const [calendarEvents, setCalendarEvents] = useState(null);
+  const [sortedSchedule, setSortedSchedule] = useState([]);
 
   useEffect(() => {
     getAllWorkoutSchedules()
@@ -17,23 +21,56 @@ const HomeScreen = () => {
 
   console.log("workoutSchedule", workoutSchedule);
 
-  const sortedSchedule = workoutSchedule.slice().sort((a, b) => a.id - b.id);
+  useEffect(() => {
+    const sortSchedule = workoutSchedule.slice().sort((a, b) => a.id - b.id);
+    setSortedSchedule(sortSchedule)
+  }, [workoutSchedule]);
+
   console.log("sortedSchedule", sortedSchedule);
 
+  useEffect(() => {
+    const events = sortedSchedule.map((data) => {
+      const inputDate = new Date(`${data.date}`);
+      const formattedDate = format(inputDate, "yyyy-MM-dd");
+      return {
+        title: data.workout_name,
+        start: formattedDate,
+        id: data.id,
+      };
+    });
+
+    setCalendarEvents(events);
+  }, [sortedSchedule]);
+
+  console.log("CALENDAR EVENTS: ", calendarEvents);
+
   return (
-    <div className="scrollableDiv">
-      {sortedSchedule.map((date, index) => {
-        const inputDate = new Date(`${date.date}`);
-        const formattedDate = format(inputDate, "EEEE do MMMM yyyy");
-        return (
-          <div key={index}>
-            <h3>{formattedDate}</h3>
-            {date.completed ? <p>Completed</p> : <p>Not Completed</p>}
-          </div>
-        );
-      })}
+    <div className="calendar">
+
+
+      <FullCalendar
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"
+      weekends={false}
+      events={calendarEvents}
+    />
+
     </div>
   );
 };
 
 export default HomeScreen;
+
+
+
+// {sortedSchedule.map((date, index) => {
+//   const inputDate = new Date(`${date.date}`);
+//   const formattedDate = format(inputDate, "EEEE dd MMMM yyyy");
+//   return (
+//     <div key={index}>
+//       <h3>{formattedDate}</h3>
+//       <h3>{date.workout_name}</h3>
+//       {date.completed ? <p>Completed</p> : <p>Not Completed</p>}
+//     </div>
+//   );
+// })}
