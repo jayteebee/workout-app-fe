07d487a675bd8a99b4f9e38fed168652457039ad
Jethro_Block <jethroblock22@gmail.COM>
2023-08-29 18:55:43 +0100

    Populate content for adding days to workouts and API call - work in progress

--- src/Components/Workout/CreateWorkout.jsx
+++ src/Components/Workout/CreateWorkout.jsx
@@ -11,38 +11,115 @@ const CreateWorkout = ({
   routineID,
   setWorkoutCreated,
 }) => {
-
-  const [formInput, setFormInput] = useState({
-    user_id: "",
-    name: "",
-  });
-  console.log("** formInput: ", formInput)
-
-  const [createdWorkout, setCreatedWorkout] = useState([]);
-  console.log("** createdWorkout: ", createdWorkout)
-
-  const [order, setOrder] = useState(0)
-
   useEffect(() => {
     const token = window.localStorage.getItem("token");
     const decodedToken = parseJwt(token);
     const userID = decodedToken.sub;
     setFormInput((prevState) => ({ ...prevState, user_id: userID }));
+    setCreateWorkoutDayData((prevState) => ({ ...prevState, user_id: userID }));
   }, [workoutToggle]);
+
+  const [formInput, setFormInput] = useState({
+    user_id: "",
+    name: "",
+  });
+  const [createdWorkout, setCreatedWorkout] = useState([]);
+  const [order, setOrder] = useState(0);
+  const [workoutDay, setWorkoutDay] = useState("");
+  const [workoutDayIndex, setWorkoutDayIndex] = useState(false);
+  console.log("** workoutDayINDEX", workoutDayIndex);
+  const [createWorkoutDayToggle, setCreateWorkoutDayToggle] = useState(false);
+
+  const [dayOfWeek, setDayOfWeek] = useState([
+    { monday: false, value: 0 },
+    { tuesday: false, value: 1 },
+    { wednesday: false, value: 2 },
+    { thursday: false, value: 3 },
+    { friday: false, value: 4 },
+    { saturday: false, value: 5 },
+    { sunday: false, value: 6 },
+  ]);
+
+  const [createWorkoutDayData, setCreateWorkoutDayData] = useState({
+    user_id: "",
+    days_of_week: [],
+    routine_id: 0,
+  });
+
+  console.log("*** createWorkoutDayData", createWorkoutDayData);
+
+  const [createDataForApiCallToggle, setCreateDataForApiCallToggle] =
+    useState(false);
+  // console.log("createDataForApiCallToggle", createDataForApiCallToggle)
+
+  const daysOfWeekArray = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+
+  useEffect(() => {
+    if (workoutDayIndex !== false) {
+      toggleDaysOfWeekBoolean(workoutDayIndex);
+    }
+  }, [workoutDayIndex]);
+
+  // useEffect(() => {
+  //   if (createDataForApiCallToggle !== null) {
+  //     createDataForApiCall();
+  //   }
+  // }, [createDataForApiCallToggle]);
+
+  const toggleDaysOfWeekBoolean = (workoutIndex) => {
+    console.log("clickedIndex INSIDE:", workoutIndex);
+
+    const updatedDaysOfWeek = dayOfWeek.map((day) => {
+      if (day.value === workoutIndex) {
+        return {
+          ...day,
+          [daysOfWeekArray[workoutIndex]]: !day[daysOfWeekArray[workoutIndex]], // toggles the boolean
+        };
+      }
+      console.log("day:", day);
+      return day;
+    });
+
+    setDayOfWeek(updatedDaysOfWeek);
+  };
+
+  console.log("dayOfWeek ***", dayOfWeek);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       let data = await createWorkout(formInput);
       setCreatedWorkout(data);
+
+      const dayNameToIndex = {
+        monday: 0,
+        tuesday: 1,
+        wednesday: 2,
+        thursday: 3,
+        friday: 4,
+        saturday: 5,
+        sunday: 6,
+      };
+
+      const lowercaseDay = workoutDay.toLowerCase();
+      if (dayNameToIndex.hasOwnProperty(lowercaseDay)) {
+        setWorkoutDayIndex(dayNameToIndex[lowercaseDay]);
+      }
     } catch (err) {
       console.error("Error:", err);
     } finally {
-      setFormInput({ user_id: "", name: ""}); 
+      setFormInput({ user_id: "", name: "" });
       setWorkoutToggle((prevState) => !prevState);
     }
   };
-
 
   useEffect(() => {
     if (createdWorkout.id) {
@@ -55,13 +132,42 @@ const CreateWorkout = ({
     }
   }, [createdWorkout, order, routineID]);
 
-
   const handleChange = (e) => {
     setFormInput({
       ...formInput,
       [e.target.name]: e.target.value,
     });
   };
+
+  // const createDataForApiCall = () => {
+  //   console.log("createDataForApiCall CALLED");
+  //   const newWorkoutDays = dayOfWeek
+  //     .filter((day) => day[daysOfWeekArray[day.value]]) // check if the boolean is true
+  //     .map((day) => day.value); // create array with true values
+  //   console.log("newWorkoutDays: ", newWorkoutDays);
+
+  //   setCreateWorkoutDayData((prevData) => ({
+  //     ...prevData,
+  //     // user_id: userID,
+  //     days_of_week: newWorkoutDays,
+  //     // routine_id: idOfRoutine,
+  //   }));
+
+  //   setCreateWorkoutDayToggle((prevState) => !prevState);
+  // };
+
+  // console.log("workoutDays: ", workoutDays);
+  // console.log("createWorkoutDayData", createWorkoutDayData);
+
+  // useEffect(() => {
+  //   createWorkoutDay(createWorkoutDayData)
+  //     .then((response) => {
+  //       console.log("Response: ", response);
+  //     })
+  //     .catch((err) => {
+  //       console.log("createWorkoutDay API Call Failed", err);
+  //     });
+  // }, [createWorkoutDayToggle]);
 
   return (
     <div>
@@ -86,13 +192,15 @@ const CreateWorkout = ({
           contrast
         />
 
-      {/* 1) Create a new input field here that will accept
-              the day of the week that workout will be done on.
-          2) Add another line to the form input state object
-              to accept dayOfWeek.
-          3) Create a function to transform Mon-Sun to 0-6.
-
-    */}
+        <MDBInput
+          className="mb-4"
+          type="text"
+          label="Workout Day"
+          value={workoutDay}
+          name="day"
+          onChange={(e) => setWorkoutDay(e.target.value)}
+          contrast
+        />
 
         <MDBBtn type="submit" className="mb-4" block>
           Create Workout
