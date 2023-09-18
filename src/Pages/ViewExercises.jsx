922a29ce98e1ca3165ab0525381938c7d7eface3
Jethro_Block <jethroblock22@gmail.COM>
2023-09-18 14:21:26 +0100

    Exercises rest timer

--- src/Pages/ViewExercises.jsx
+++ src/Pages/ViewExercises.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getExercisesInWorkout } from "../API/Workout/Workout";
-import "../CSS/Exercises.css"
+import "../CSS/Exercises.css";
 
 const ViewExercises = () => {
   const location = useLocation();
   const workoutID = location.state?.workoutId;
 
   const [exercises, setExercises] = useState([]);
-  console.log("Exercises", exercises)
+  console.log("Exercises", exercises);
   useEffect(() => {
     if (workoutID) {
       getExercisesInWorkout(workoutID)
@@ -24,17 +24,15 @@ const ViewExercises = () => {
   return (
     <div>
       <h3 className="pageHeader">Exercises</h3>
-<div className="scrollableDivForViewExercises">
-      {exercises.map((exercises, index) => (
-        <div  key={index}>
-          <p>{exercises.exercise.name}</p>
-          <p>Sets: {exercises.sets}</p>
-           <p>Reps: {exercises.reps}</p>
-           <p>Rest: {exercises.rest_timer} seconds</p>
-
-          
-        </div>
-      ))}
+      <div className="scrollableDivForViewExercises">
+        {exercises.map((exercises, index) => (
+          <div key={index}>
+            <p>{exercises.exercise.name}</p>
+            <p>Sets: {exercises.sets}</p>
+            <p>Reps: {exercises.reps}</p>
+            <p>Rest: {exercises.rest_timer} seconds</p>
+          </div>
+        ))}
       </div>
     </div>
   );
