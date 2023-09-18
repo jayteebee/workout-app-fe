08108a1cfc0b3f213d3b8c05fa7f68e4ec1be5fb
Jethro_Block <jethroblock22@gmail.COM>
2023-09-18 12:52:26 +0100

    Workout Name Rendering

--- src/Pages/WorkoutSession.jsx
+++ src/Pages/WorkoutSession.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import StopWatch from "../Components/WorkoutSession/StopWatch";
 import greenRhombus from "../CSS/Icons/GreenRhombus.png";
@@ -7,41 +7,44 @@ import purpleRhombus from "../CSS/Icons/purpleRhombus.png";
 
 const WorkoutSession = () => {
   const [buttonColor, setButtonColor] = useState("green");
-  console.log("buttonColor", buttonColor);
-  const [expandDiv, setExpandDiv] = useState(false)
-  console.log("expandDiv", expandDiv);
-
+  // console.log("buttonColor", buttonColor);
+  const [expandDiv, setExpandDiv] = useState(false);
+  // console.log("expandDiv", expandDiv);
 
   const location = useLocation();
   const exercisesInWorkout = location.state?.exercisesInWorkout;
-  console.log("exercisesInWorkout", exercisesInWorkout);
+  // console.log("exercisesInWorkout", exercisesInWorkout);
 
   const exerciseButton = (value) => {
     setButtonColor("");
     setButtonColor(value);
   };
 
-const moreExerciseInfo = () => {
-setExpandDiv(prevState => !prevState)
-}
+  const moreExerciseInfo = () => {
+    setExpandDiv((prevState) => !prevState);
+  };
 
-  const workoutName = exercisesInWorkout.map((exercise) => (
-    <div key={exercise.id}>
-      <h2>Workout Name: {exercise.workout_name}</h2>
-    </div>
-  ));
+  const workoutName = (
+    <h2>Workout Name: {exercisesInWorkout[0].workout_name}</h2>
+  );
 
   const displayWorkoutData = exercisesInWorkout.map((exercise, i) => (
     <div key={exercise.id} className="exerciseSession">
       <div className="exerciseInfo">
-
-        <div style={{width: "5%"}}>
+        <div style={{ width: "5%" }}>
           <p>
             {i + 1} {")"}
           </p>
         </div>
 
-        <div className={ expandDiv ? "exerciseSessionBorderExpanded" : "exerciseSessionBorder"} onClick={moreExerciseInfo}>
+        <div
+          className={
+            expandDiv
+              ? "exerciseSessionBorderExpanded"
+              : "exerciseSessionBorder"
+          }
+          onClick={moreExerciseInfo}
+        >
           <p>{exercise.exercise.name}</p>
           <p>Sets {exercise.sets}</p>
           <p>Reps {exercise.reps}</p>
@@ -65,7 +68,6 @@ setExpandDiv(prevState => !prevState)
           </button>
         ) : null}
       </div>
-
     </div>
   ));
   console.log("displayWorkoutData", displayWorkoutData);
