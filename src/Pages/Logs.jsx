19572c1fec1c0ccb57ab4e1cef280167f29ae9b5
Jethro_Block <jethroblock22@gmail.COM>
2023-09-19 22:47:04 +0100

    getAllSessionLogs api call

--- src/Pages/Logs.jsx
+++ src/Pages/Logs.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllSessionLogs } from '../API/SessionLogs/SessionLogs'
 
 const Logs = () => {
+const [sessionLogs, setSessionLogs] = useState([])
+console.log("sessionLogs", sessionLogs)
+  useEffect(() => {
+    getAllSessionLogs()
+    .then((data) => {
+      setSessionLogs(data)
+    })
+    .catch((err) => console.log("Error with getAllSessionLogs API Call", err))
+  }, [])
+
+
   return (
-    <div>Logs</div>
+    <div>
+    <h2>Logs</h2>
+
+    </div>
   )
 }
 
