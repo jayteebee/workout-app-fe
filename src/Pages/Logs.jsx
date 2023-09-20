bddfe67fed928175b515e6c6e2df234bc4b49991
Jethro_Block <jethroblock22@gmail.COM>
2023-09-20 14:43:09 +0100

    formatting

--- src/Pages/Logs.jsx
+++ src/Pages/Logs.jsx
@@ -1,39 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { getAllSessionLogs } from '../API/SessionLogs/SessionLogs'
+import React, { useEffect, useState } from "react";
+import { getAllSessionLogs } from "../API/SessionLogs/SessionLogs";
 
 const Logs = () => {
-const [sessionLogs, setSessionLogs] = useState([])
-console.log("sessionLogs", sessionLogs)
+  const [sessionLogs, setSessionLogs] = useState([]);
+  console.log("sessionLogs", sessionLogs);
   useEffect(() => {
     getAllSessionLogs()
-    .then((data) => {
-      setSessionLogs(data)
-    })
-    .catch((err) => console.log("Error with getAllSessionLogs API Call", err))
-  }, [])
+      .then((data) => {
+        setSessionLogs(data);
+      })
+      .catch((err) =>
+        console.log("Error with getAllSessionLogs API Call", err)
+      );
+  }, []);
 
-
-const workoutLogs = sessionLogs.length > 0 && sessionLogs.map((log, index) => (
-<div key={index}>
-  <h4>Workout: {log.details.routine_workout_id}</h4>
-  <h4>Date Completed: {log.details.date}</h4>
-  {log.details.exercise_sessions.map((exercise, exerciseIndex) => (
-    <div key={exerciseIndex}>
-      <p>Set: {exercise.sets_completed}</p>
-      <p>Reps: {exercise.reps_completed}</p>
-      <p>Weight: {exercise.weight_used}</p>
-      <p>Set Duration: {exercise.set_timer} seconds</p>
-    </div>
-  ))}
-</div>
-))
+  const workoutLogs =
+    sessionLogs.length > 0 &&
+    sessionLogs.map((log, index) => (
+      <div key={index}>
+        <h4>Workout: {log.details.routine_workout_id}</h4>
+        <h4>Date Completed: {log.details.date}</h4>
+        {log.details.exercise_sessions.map((exercise, exerciseIndex) => (
+          <div key={exerciseIndex}>
+            <p>Set: {exercise.sets_completed}</p>
+            <p>Reps: {exercise.reps_completed}</p>
+            <p>Weight: {exercise.weight_used}</p>
+            <p>Set Duration: {exercise.set_timer} seconds</p>
+          </div>
+        ))}
+      </div>
+    ));
 
   return (
     <div>
-    <h2>Logs</h2>
-{workoutLogs}
+      <h2>Logs</h2>
+      {workoutLogs}
     </div>
-  )
-}
+  );
+};
 
-export default Logs
+export default Logs;
