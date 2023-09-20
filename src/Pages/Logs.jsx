d089f80587e452e811ad442bac87e1ee6cedf504
Jethro_Block <jethroblock22@gmail.COM>
2023-09-20 18:42:36 +0100

    log logic

--- src/Pages/Logs.jsx
+++ src/Pages/Logs.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { getAllSessionLogs } from "../API/SessionLogs/SessionLogs";
+import { getAllExercises } from "../API/Exercise/Exercise";
 
 const Logs = () => {
   const [sessionLogs, setSessionLogs] = useState([]);
-  console.log("sessionLogs", sessionLogs);
+  // console.log("sessionLogs", sessionLogs);
+  const [allExercises, setAllExercises] = useState([])
+  // console.log("allExercises", allExercises);
+
   useEffect(() => {
     getAllSessionLogs()
       .then((data) => {
@@ -11,6 +15,13 @@ const Logs = () => {
       })
       .catch((err) =>
         console.log("Error with getAllSessionLogs API Call", err)
+      );
+      getAllExercises()
+      .then((exData) => {
+        setAllExercises(exData);
+      })
+      .catch((error) =>
+        console.log("Error with getAllExercises API Call", error)
       );
   }, []);
 
@@ -20,14 +31,22 @@ const Logs = () => {
       <div key={index}>
         <h4>Workout: {log.details.routine_workout_id}</h4>
         <h4>Date Completed: {log.details.date}</h4>
-        {log.details.exercise_sessions.map((exercise, exerciseIndex) => (
+        {log.details.exercise_sessions.map((exercise, exerciseIndex) => {
+          const filteredExercise = allExercises.length > 0 && allExercises.filter((ex) => ex.id === exercise.exercise_id)
+          return (
           <div key={exerciseIndex}>
+          {filteredExercise && (
+            <div>
+            <p>Exercise: {filteredExercise[0].name}</p>
             <p>Set: {exercise.sets_completed}</p>
             <p>Reps: {exercise.reps_completed}</p>
             <p>Weight: {exercise.weight_used}</p>
             <p>Set Duration: {exercise.set_timer} seconds</p>
+            </div>
+          )}
           </div>
-        ))}
+          )
+        })}
       </div>
     ));
 
