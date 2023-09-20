61948d9ab964b9e3b741f6fee898089d26c975e3
Jethro_Block <jethroblock22@gmail.COM>
2023-09-20 14:42:26 +0100

    Render session log info

--- src/Pages/Logs.jsx
+++ src/Pages/Logs.jsx
@@ -13,10 +13,25 @@ console.log("sessionLogs", sessionLogs)
   }, [])
 
 
+const workoutLogs = sessionLogs.length > 0 && sessionLogs.map((log, index) => (
+<div key={index}>
+  <h4>Workout: {log.details.routine_workout_id}</h4>
+  <h4>Date Completed: {log.details.date}</h4>
+  {log.details.exercise_sessions.map((exercise, exerciseIndex) => (
+    <div key={exerciseIndex}>
+      <p>Set: {exercise.sets_completed}</p>
+      <p>Reps: {exercise.reps_completed}</p>
+      <p>Weight: {exercise.weight_used}</p>
+      <p>Set Duration: {exercise.set_timer} seconds</p>
+    </div>
+  ))}
+</div>
+))
+
   return (
     <div>
     <h2>Logs</h2>
-
+{workoutLogs}
     </div>
   )
 }
