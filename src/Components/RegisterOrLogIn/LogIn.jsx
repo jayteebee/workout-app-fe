97da17a0ad350a91851518afe8f86a258c79d628
Jethro_Block <jethroblock22@gmail.COM>
2023-09-21 15:36:11 +0100

    contrast on forms

--- src/Components/RegisterOrLogIn/LogIn.jsx
+++ src/Components/RegisterOrLogIn/LogIn.jsx
@@ -1,73 +1,91 @@
-import React, {useState} from 'react'
-import { useNavigate } from 'react-router-dom';
-import { logIn } from '../../API/Authentication/Authentication';
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { logIn } from "../../API/Authentication/Authentication";
 
 import {
-    MDBInput,
-    MDBCol,
-    MDBRow,
-    MDBCheckbox,
-    MDBBtn
-  } from 'mdb-react-ui-kit';
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
-const LogIn = ({setShowRegister, setLoggedIn}) => {
-  const [formInput, setFormInput] = useState({email: "", password:""})
-const navigate = useNavigate()
+const LogIn = ({ setShowRegister, setLoggedIn }) => {
+  const [formInput, setFormInput] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await logIn(formInput);
-      setLoggedIn(true)
+      setLoggedIn(true);
     } catch (err) {
-      console.error("Error Logging In: ",err);
+      console.error("Error Logging In: ", err);
     } finally {
-      setFormInput({email: "", password: ""});
+      setFormInput({ email: "", password: "" });
     }
-    setShowRegister(true)
-    navigate("/")
-  }
-    
+    setShowRegister(true);
+    navigate("/");
+  };
+
   const handleChange = (e) => {
     setFormInput({
       ...formInput,
       [e.target.name]: e.target.value,
-    })
+    });
   };
 
   const showRegister = (e) => {
     e.preventDefault();
-    setShowRegister(true)
-  }
+    setShowRegister(true);
+  };
 
   return (
     <div>
-    <form onSubmit={handleSubmit}>
-      <MDBInput className='mb-4' type='email' label='Email address' value={formInput.email} name="email" onChange={handleChange} />
-      <MDBInput className='mb-4' type='password' label='Password' value={formInput.password} name="password" onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <MDBInput
+          className="mb-4"
+          type="email"
+          label="Email address"
+          value={formInput.email}
+          name="email"
+          onChange={handleChange}
+          contrast
+        />
+        <MDBInput
+          className="mb-4"
+          type="password"
+          label="Password"
+          value={formInput.password}
+          name="password"
+          onChange={handleChange}
+          contrast
+        />
 
-      <MDBRow className='mb-4'>
-        <MDBCol className='d-flex justify-content-center'>
-          <MDBCheckbox label='Remember me' defaultChecked />
-        </MDBCol>
-        <MDBCol>
-          <a href='#!'>Forgot password?</a>
-        </MDBCol>
-      </MDBRow>
+        <MDBRow className="mb-4">
+          <MDBCol className="d-flex justify-content-center">
+            <MDBCheckbox label="Remember me" defaultChecked />
+          </MDBCol>
+          <MDBCol>
+            <a href="#!">Forgot password?</a>
+          </MDBCol>
+        </MDBRow>
 
-      <MDBBtn type='submit' className='mb-4' block>
-        Sign in
-      </MDBBtn>
+        <MDBBtn type="submit" className="mb-4" block>
+          Sign in
+        </MDBBtn>
 
-      <div className='text-center'>
-        <p>
-          Not a member? <a href="#!" onClick={showRegister}>Register</a>
-        </p>
-      </div>
-    </form>
+        <div className="text-center">
+          <p>
+            Not a member?{" "}
+            <a href="#!" onClick={showRegister}>
+              Register
+            </a>
+          </p>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default LogIn;
--- src/Components/RegisterOrLogIn/Register.jsx
+++ src/Components/RegisterOrLogIn/Register.jsx
@@ -1,61 +1,76 @@
-import React from 'react'
-import {
-    MDBInput,
-    MDBRow,
-    MDBBtn
-  } from 'mdb-react-ui-kit';
-import { useState } from 'react';
-import { createUser } from '../../API/Authentication/Authentication';
-  
-const Register = ({setShowRegister}) => {
-const [formInput, setFormInput] = useState({email: "", password:""})
+import React from "react";
+import { MDBInput, MDBRow, MDBBtn } from "mdb-react-ui-kit";
+import { useState } from "react";
+import { createUser } from "../../API/Authentication/Authentication";
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    await createUser(formInput);
-  } catch (err) {
-    console.error("Error Creating User: ",err);
-  } finally {
-    setFormInput({email: "", password: ""});
-  }
-  setShowRegister(false)
-}
+const Register = ({ setShowRegister }) => {
+  const [formInput, setFormInput] = useState({ email: "", password: "" });
 
-    const handlechange = (e) => {
-      setFormInput({
-        ...formInput,
-        [e.target.name]: e.target.value,
-      })
-    };
-
-    const showRegister = (e) => {
-      e.preventDefault();
-      setShowRegister(false)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createUser(formInput);
+    } catch (err) {
+      console.error("Error Creating User: ", err);
+    } finally {
+      setFormInput({ email: "", password: "" });
     }
+    setShowRegister(false);
+  };
+
+  const handlechange = (e) => {
+    setFormInput({
+      ...formInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const showRegister = (e) => {
+    e.preventDefault();
+    setShowRegister(false);
+  };
 
   return (
     <div>
-    <form onSubmit={handleSubmit}>
-      <MDBRow className='mb-4'>
-      </MDBRow>
-      <MDBInput className='mb-4' type='email' label='Email address' value={formInput.email} name="email" onChange={handlechange} />
-      <MDBInput className='password' type='password'  label='Password' value={formInput.password} name="password" onChange={handlechange} />
-      <p className='passWordAuthMessage'>Password must be longer than 6 characters.</p>
+      <form onSubmit={handleSubmit}>
+        <MDBRow className="mb-4"></MDBRow>
+        <MDBInput
+          className="mb-4"
+          type="email"
+          label="Email address"
+          value={formInput.email}
+          name="email"
+          onChange={handlechange}
+          contrast
+        />
+        <MDBInput
+          className="password"
+          type="password"
+          label="Password"
+          value={formInput.password}
+          name="password"
+          onChange={handlechange}
+          contrast
+        />
+        <p className="passWordAuthMessage">
+          Password must be longer than 6 characters.
+        </p>
 
+        <MDBBtn type="submit" className="mb-4" block>
+          Sign Up!
+        </MDBBtn>
 
-      <MDBBtn type='submit' className='mb-4' block >
-        Sign Up!
-      </MDBBtn>
-
-      <div className='text-center'>
-      <p>
-      Already a member? <a href="#!" onClick={showRegister} >Log In</a>
-    </p>
-      </div>
-    </form>
+        <div className="text-center">
+          <p>
+            Already a member?{" "}
+            <a href="#!" onClick={showRegister}>
+              Log In
+            </a>
+          </p>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
