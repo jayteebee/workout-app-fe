7ec05d30531ec787922f26d60da6f8adbe1c23d3
Jethro_Block <jethroblock22@gmail.COM>
2023-09-21 15:19:06 +0100

    Backend URL

--- src/API/AxiosInstances/axiosInstance.js
+++ src/API/AxiosInstances/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://workout-app-web-service.onrender.com'//'https://workout-app-backend-a67feb525b8a.herokuapp.com' 
+    ? 'https://workout-app-backend-a67feb525b8a.herokuapp.com' 
                 : 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json'
