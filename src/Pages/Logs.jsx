13ada12d96a98b2004a79d68db833fc9f89f2125
Jethro_Block <jethroblock22@gmail.COM>
2023-09-25 16:47:23 +0100

    styling and sort by date

--- src/Pages/Logs.jsx
+++ src/Pages/Logs.jsx
@@ -4,7 +4,7 @@ import { getAllExercises } from "../API/Exercise/Exercise";
 import { getAllWorkouts } from "../API/Workout/Workout";
 import format from "date-fns/format";
 import { getTime, parseISO } from 'date-fns'
-
+import "../CSS/Logs.css"
 
 
 const Logs = () => {
@@ -13,7 +13,11 @@ const Logs = () => {
   const [allExercises, setAllExercises] = useState([])
   // console.log("allExercises", allExercises);
   const [allWorkouts, setAllWorkouts] = useState([])
-console.log('allWorkouts', allWorkouts)
+// console.log('allWorkouts', allWorkouts)
+
+const sortedSessionLogs = sessionLogs.sort((a, b) =>
+  new Date(b.details.date) - new Date(a.details.date)
+);
 
   useEffect(() => {
     getAllSessionLogs()
@@ -40,8 +44,8 @@ console.log('allWorkouts', allWorkouts)
   }, []);
 
   const workoutLogs =
-    sessionLogs.length > 0 &&
-    sessionLogs.map((log, index) => {
+  sortedSessionLogs.length > 0 &&
+  sortedSessionLogs.map((log, index) => {
       const filteredWorkout = allWorkouts.length > 0 && allWorkouts.filter((workout) => workout.id === log.details.routine_workout_id );
       const inputDate = new Date(`${log.details.date}`);
       const formattedDate = format(inputDate, "yyyy-MM-dd");
@@ -50,21 +54,21 @@ console.log('allWorkouts', allWorkouts)
       const dateToWords = format(parsedDate, "EEEE, do MMMM yyyy");
 
       return (
-      <div key={index}>
-        <h2>Workout: {filteredWorkout[0].name}</h2>
+      <div key={index} className="workoutLog">
+        <h2 className="workoutName">Workout: {filteredWorkout && filteredWorkout[0].name}</h2>
         <h4>Date Completed: {dateToWords} {formattedTime} </h4>
 
         {log.details.exercise_sessions.map((exercise, exerciseIndex) => {
           const filteredExercise = allExercises.length > 0 && allExercises.filter((ex) => ex.id === exercise.exercise_id)
-          return (
 
+          return (
           <div key={exerciseIndex}>
           {filteredExercise && (
-            <div>
-            <p>Exercise: {filteredExercise[0].name}</p>
+            <div className="exerciseLog">
+            <p>{filteredExercise[0].name}:</p>
             <p>Set: {exercise.sets_completed}</p>
             <p>Reps: {exercise.reps_completed}</p>
-            <p>Weight: {exercise.weight_used}</p>
+            <p>Weight: {exercise.weight_used}kg</p>
             <p>Set Duration: {exercise.set_timer} seconds</p>
             </div>
           )}
@@ -78,9 +82,12 @@ console.log('allWorkouts', allWorkouts)
       });
 
   return (
-    <div>
-      <h2 style={{textDecoration: "underline"}}>Logs</h2>
+    <div  className="grid-container">
+      <h2 className="pageHeader logs">Logs</h2>
+      <div className="workoutLogs">
       {workoutLogs}
+      </div>
+      
 
     </div>
   );
