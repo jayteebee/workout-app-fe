801d0950bfef342ebc7a3e8732d092b2fc89d24e
Jethro_Block <jethroblock22@gmail.COM>
2023-11-27 10:35:12 +0000

    allExerciseNamesArray

--- src/Components/Workout Logs/WorkoutExerciseFilter.jsx
+++ src/Components/Workout Logs/WorkoutExerciseFilter.jsx
@@ -2,6 +2,12 @@ import { MDBBtn } from 'mdb-react-ui-kit'
 import React from 'react'
 
 const WorkoutExerciseFilter = ({sortedSessionLogs, allExercises }) => {
+  console.log('sortedSessionLogs',sortedSessionLogs)
+  console.log('allExercises',allExercises)
+
+  const allExerciseNames = allExercises.map((exercise) => exercise.name)
+  console.log('allExerciseNames',allExerciseNames)
+
   return (
     <div>
     <MDBBtn>Exercise In Workout</MDBBtn>
