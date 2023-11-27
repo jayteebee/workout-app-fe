80ec3463b2d1e42d3e960292b597c8c13b75f84b
Jethro_Block <jethroblock22@gmail.COM>
2023-11-27 17:20:22 +0000

    filter logs via exercise name

--- src/Components/Workout Logs/WorkoutExerciseFilter.jsx
+++ src/Components/Workout Logs/WorkoutExerciseFilter.jsx
@@ -1,20 +1,33 @@
 import { MDBBtn } from "mdb-react-ui-kit";
 import ReactSelect from "react-select";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const WorkoutExerciseFilter = ({ sortedSessionLogs, allExercises }) => {
   console.log("sortedSessionLogs", sortedSessionLogs);
   // console.log('allExercises',allExercises)
   const [chosenExerciseToFilter, setChosenExerciseToFilter] = useState("")
-console.log('chosenExerciseToFilter',chosenExerciseToFilter)
-
+// console.log('chosenExerciseToFilter',chosenExerciseToFilter)
 
   const allExerciseNames = allExercises.map((exercise) => ({
-    value: exercise.name,
+    value: exercise.id,
     label: exercise.name,
   }));
   // console.log('allExerciseNames',allExerciseNames)
+
+
+  useEffect(() => {
+    if (chosenExerciseToFilter) {
+      const sessionLogsByChosenExerciseFilter = sortedSessionLogs.filter((log) =>
+        log.details.exercise_sessions.some(
+          (exerciseSession) => exerciseSession.id === chosenExerciseToFilter
+        )
+      );
+      console.log('sessionLogsByChosenExerciseFilter', sessionLogsByChosenExerciseFilter);
+      setSessionLogsByChosenName(sessionLogsByChosenExerciseFilter);
+
+    }
+  }, [chosenExerciseToFilter]);
 
   return (
     <div>
