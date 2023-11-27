7a090a4956ea265046fbf6147138db8d79860352
Jethro_Block <jethroblock22@gmail.COM>
2023-11-27 16:48:25 +0000

    add react select

--- src/Components/Workout Logs/WorkoutExerciseFilter.jsx
+++ src/Components/Workout Logs/WorkoutExerciseFilter.jsx
@@ -1,16 +1,26 @@
 import { MDBBtn } from 'mdb-react-ui-kit'
+import ReactSelect from "react-select";
+
 import React from 'react'
 
 const WorkoutExerciseFilter = ({sortedSessionLogs, allExercises }) => {
   console.log('sortedSessionLogs',sortedSessionLogs)
   console.log('allExercises',allExercises)
 
-  const allExerciseNames = allExercises.map((exercise) => exercise.name)
+  const allExerciseNames = allExercises.map((exercise) => ({
+    value: exercise.name,
+    label: exercise.name
+  }))
   console.log('allExerciseNames',allExerciseNames)
+
 
   return (
     <div>
-    <MDBBtn>Exercise In Workout</MDBBtn>
+
+<ReactSelect
+options={allExerciseNames}
+/>
+
     </div>
   )
 }
