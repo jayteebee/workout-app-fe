0aacca617b7c0551fc2413886c76171c9a0ff41f
Jethro_Block <jethroblock22@gmail.COM>
2023-11-27 16:48:48 +0000

    formatting

--- src/Components/Workout Logs/WorkoutExerciseFilter.jsx
+++ src/Components/Workout Logs/WorkoutExerciseFilter.jsx
@@ -1,28 +1,23 @@
-import { MDBBtn } from 'mdb-react-ui-kit'
+import { MDBBtn } from "mdb-react-ui-kit";
 import ReactSelect from "react-select";
 
-import React from 'react'
+import React from "react";
 
-const WorkoutExerciseFilter = ({sortedSessionLogs, allExercises }) => {
-  console.log('sortedSessionLogs',sortedSessionLogs)
-  console.log('allExercises',allExercises)
+const WorkoutExerciseFilter = ({ sortedSessionLogs, allExercises }) => {
+  console.log("sortedSessionLogs", sortedSessionLogs);
+  // console.log('allExercises',allExercises)
 
   const allExerciseNames = allExercises.map((exercise) => ({
     value: exercise.name,
-    label: exercise.name
-  }))
-  console.log('allExerciseNames',allExerciseNames)
-
+    label: exercise.name,
+  }));
+  // console.log('allExerciseNames',allExerciseNames)
 
   return (
     <div>
-
-<ReactSelect
-options={allExerciseNames}
-/>
-
+      <ReactSelect options={allExerciseNames} />
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutExerciseFilter
+export default WorkoutExerciseFilter;
