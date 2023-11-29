b36ea1be900f3775b68a1a0897ce33d01c075cb8
Jethro_Block <jethroblock22@gmail.COM>
2023-11-29 13:29:06 +0000

    formatting

--- src/Components/Workout Logs/WorkoutExerciseFilter.jsx
+++ src/Components/Workout Logs/WorkoutExerciseFilter.jsx
@@ -3,43 +3,40 @@ import ReactSelect from "react-select";
 
 import React, { useEffect, useState } from "react";
 
-const WorkoutExerciseFilter = ({ sortedSessionLogs, allExercises, setSessionLogsByChosenExercise, setSessionLogsByChosenName }) => {
-  console.log("sortedSessionLogs", sortedSessionLogs);
-  // console.log('allExercises',allExercises)
-  const [chosenExerciseToFilter, setChosenExerciseToFilter] = useState("")
-// console.log('chosenExerciseToFilter',chosenExerciseToFilter)
+const WorkoutExerciseFilter = ({
+  sortedSessionLogs,
+  allExercises,
+  setSessionLogsByChosenExercise,
+  setSessionLogsByChosenName,
+}) => {
+  const [chosenExerciseToFilter, setChosenExerciseToFilter] = useState("");
 
   const allExerciseNames = allExercises.map((exercise) => ({
     value: exercise.id,
     label: exercise.name,
   }));
-  console.log('allExerciseNames',allExerciseNames) 
-
-const [temp, setTemp] = useState([])
-console.log('temp',temp)
 
   useEffect(() => {
     if (chosenExerciseToFilter) {
-      const sessionLogsByChosenExerciseFilter = sortedSessionLogs.filter((log) =>
-        log.details.exercise_sessions.some(
-          (exerciseSession) => exerciseSession.exercise_id === chosenExerciseToFilter
-        )
+      const sessionLogsByChosenExerciseFilter = sortedSessionLogs.filter(
+        (log) =>
+          log.details.exercise_sessions.some(
+            (exerciseSession) =>
+              exerciseSession.exercise_id === chosenExerciseToFilter
+          )
       );
-      console.log('sessionLogsByChosenExerciseFilter',sessionLogsByChosenExerciseFilter)
-      setTemp(sessionLogsByChosenExerciseFilter)
       setSessionLogsByChosenExercise(sessionLogsByChosenExerciseFilter);
-
     }
   }, [chosenExerciseToFilter]);
 
   return (
     <div>
-      <ReactSelect 
-      options={allExerciseNames}
-      onChange={(e) => {
-        setSessionLogsByChosenName(null);
-        setChosenExerciseToFilter(e.value);
-      }}
+      <ReactSelect
+        options={allExerciseNames}
+        onChange={(e) => {
+          setSessionLogsByChosenName(null);
+          setChosenExerciseToFilter(e.value);
+        }}
       />
     </div>
   );
