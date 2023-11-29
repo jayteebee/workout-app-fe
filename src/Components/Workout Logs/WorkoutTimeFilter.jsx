a8820ac8436d8f1914d76a0e2dcd8d0d7154bcb1
Jethro_Block <jethroblock22@gmail.COM>
2023-11-29 14:13:57 +0000

    logic for date filtering

--- src/Components/Workout Logs/WorkoutTimeFilter.jsx
+++ src/Components/Workout Logs/WorkoutTimeFilter.jsx
@@ -1,10 +1,52 @@
-import { MDBBtn } from 'mdb-react-ui-kit'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DatePicker from "react-datepicker";
+
+
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const WorkoutTimeFilter = ({sortedSessionLogs}) => {
+
+const [chosenDate, setChosenDate] = useState("");
+const [monthChosenByUser, setMonthChosenByUser] = useState("")
+const [filteredLogsByMonth, setFilteredLogsByMonth] = useState([])
+console.log('filteredLogsByMonth',filteredLogsByMonth)
+
+useEffect(() => {
+  if (chosenDate) {
+    const dateToFormat = chosenDate
+    const monthName = dateToFormat.toLocaleString('default', {month: "long"})
+    setMonthChosenByUser(monthName)
+  }
+}, [chosenDate])
+
+useEffect(() => {
+  if (monthChosenByUser) {
+    const filterLogsByMonth = sortedSessionLogs.filter((log) => 
+    {
+      const date = log.details.date
+      const dateToCompare = new Date(date)
+      const monthName = dateToCompare.toLocaleString('default', {month: "long"})
+
+      return (
+        monthChosenByUser === monthName
+      )
+    }
+    )
+    setFilteredLogsByMonth(filterLogsByMonth)
+  }
+}, [monthChosenByUser])
+
   return (
     <div>
-    <MDBBtn>Workout Time</MDBBtn>
+    <DatePicker 
+    selected={new Date()} 
+    onChange={(date) => setChosenDate(date)}
+    dateFormat="MMMM/yyyy"
+    showMonthYearPicker
+    showFullMonthYearPicker
+    />
+
     </div>
   )
 }
