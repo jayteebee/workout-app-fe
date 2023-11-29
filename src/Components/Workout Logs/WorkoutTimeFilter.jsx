321b6003ced2763d49c28f9c8a6812f79b9421f9
Jethro_Block <jethroblock22@gmail.COM>
2023-11-29 14:16:46 +0000

    formatting

--- src/Components/Workout Logs/WorkoutTimeFilter.jsx
+++ src/Components/Workout Logs/WorkoutTimeFilter.jsx
@@ -1,58 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
-
-
 
 import "react-datepicker/dist/react-datepicker.css";
 
-const WorkoutTimeFilter = ({sortedSessionLogs, setSessionLogsByChosenDate}) => {
+const WorkoutTimeFilter = ({
+  sortedSessionLogs,
+  setSessionLogsByChosenDate,
+}) => {
+  const [chosenDate, setChosenDate] = useState("");
+  const [monthChosenByUser, setMonthChosenByUser] = useState("");
+  const [filteredLogsByMonth, setFilteredLogsByMonth] = useState([]);
 
-const [chosenDate, setChosenDate] = useState("");
-const [monthChosenByUser, setMonthChosenByUser] = useState("")
-const [filteredLogsByMonth, setFilteredLogsByMonth] = useState([])
-console.log('filteredLogsByMonth',filteredLogsByMonth)
-
-useEffect(() => {
-  if (chosenDate) {
-    const dateToFormat = chosenDate
-    const monthName = dateToFormat.toLocaleString('default', {month: "long"})
-    setMonthChosenByUser(monthName)
-  }
-}, [chosenDate])
-
-useEffect(() => {
-  if (monthChosenByUser) {
-    const filterLogsByMonth = sortedSessionLogs.filter((log) => 
-    {
-      const date = log.details.date
-      const dateToCompare = new Date(date)
-      const monthName = dateToCompare.toLocaleString('default', {month: "long"})
-
-      return (
-        monthChosenByUser === monthName
-      )
+  useEffect(() => {
+    if (chosenDate) {
+      const dateToFormat = chosenDate;
+      const monthName = dateToFormat.toLocaleString("default", {
+        month: "long",
+      });
+      setMonthChosenByUser(monthName);
     }
-    )
-    setFilteredLogsByMonth(filterLogsByMonth)
-  }
-}, [monthChosenByUser])
+  }, [chosenDate]);
 
-useEffect(() => {
-  setSessionLogsByChosenDate(filteredLogsByMonth)
-}, [filteredLogsByMonth])
+  useEffect(() => {
+    if (monthChosenByUser) {
+      const filterLogsByMonth = sortedSessionLogs.filter((log) => {
+        const date = log.details.date;
+        const dateToCompare = new Date(date);
+        const monthName = dateToCompare.toLocaleString("default", {
+          month: "long",
+        });
+
+        return monthChosenByUser === monthName;
+      });
+      setFilteredLogsByMonth(filterLogsByMonth);
+    }
+  }, [monthChosenByUser]);
+
+  useEffect(() => {
+    setSessionLogsByChosenDate(filteredLogsByMonth);
+  }, [filteredLogsByMonth]);
 
   return (
     <div>
-    <DatePicker 
-    selected={new Date()} 
-    onChange={(date) => setChosenDate(date)}
-    dateFormat="MMMM/yyyy"
-    showMonthYearPicker
-    showFullMonthYearPicker
-    />
-
+      <DatePicker
+        selected={new Date()}
+        onChange={(date) => setChosenDate(date)}
+        dateFormat="MMMM/yyyy"
+        showMonthYearPicker
+        showFullMonthYearPicker
+      />
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutTimeFilter
+export default WorkoutTimeFilter;
