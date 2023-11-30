fb89a91854764ae3dce680283ca009c667eac53c
Jethro_Block <jethroblock22@gmail.COM>
2023-11-30 10:30:57 +0000

    fetch all session logs for analytics

--- src/Pages/Analytics.jsx
+++ src/Pages/Analytics.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllSessionLogs } from '../API/SessionLogs/SessionLogs'
 
 const Analytics = () => {
+
+const [allSessionLogs, setAllSessionLogs] = useState()
+console.log('allSessionLogs',allSessionLogs)
+
+useEffect(() => {
+  getAllSessionLogs()
+  .then((data) => setAllSessionLogs(data))
+  .catch((err) => console.log("Error Fetching All Session Logs", err))
+}, [])
+
   return (
     <div>Analytics</div>
   )
