c7d6c39596aeee7b88ab87fef996e289cf880eeb
Jethro_Block <jethroblock22@gmail.COM>
2023-11-30 10:35:59 +0000

    sort session logs via date

--- src/Pages/Analytics.jsx
+++ src/Pages/Analytics.jsx
@@ -4,13 +4,19 @@ import { getAllSessionLogs } from '../API/SessionLogs/SessionLogs'
 const Analytics = () => {
 
 const [allSessionLogs, setAllSessionLogs] = useState()
-console.log('allSessionLogs',allSessionLogs)
+// console.log('allSessionLogs',allSessionLogs)
 
 useEffect(() => {
   getAllSessionLogs()
   .then((data) => setAllSessionLogs(data))
   .catch((err) => console.log("Error Fetching All Session Logs", err))
 }, [])
+
+const sortedSessionLogs = allSessionLogs && allSessionLogs.length > 0 && allSessionLogs.sort(
+  (a,b) => new Date(b.details.date) - new Date(a.details.date)
+)
+console.log('sortedSessionLogs',sortedSessionLogs)
+
 
   return (
     <div>Analytics</div>
