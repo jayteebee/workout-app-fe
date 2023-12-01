99d0e2f2ac8a498c69469518c12b63fd07028d5d
Jethro_Block <jethroblock22@gmail.COM>
2023-12-01 12:54:52 +0000

    create TotalVolumeByChosenTimeFrame comp

---
+++ src/Components/Analytics/TotalVolumeByChosenTimeFrame.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TotalVolumeByChosenTimeFrame = () => {
+  return (
+    <div>TotalVolumeByChosenTimeFrame</div>
+  )
+}
+
+export default TotalVolumeByChosenTimeFrame
