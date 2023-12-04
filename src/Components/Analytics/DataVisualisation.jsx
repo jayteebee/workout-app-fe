c260b783ec138c6b405eedb59ca07dcf8dc65d2b
Jethro_Block <jethroblock22@gmail.COM>
2023-12-04 16:26:09 +0000

    formatting

--- src/Components/Analytics/DataVisualisation.jsx
+++ src/Components/Analytics/DataVisualisation.jsx
@@ -3,7 +3,7 @@ import DatePicker, { registerLocale } from "react-datepicker";
 import enGB from "date-fns/locale/en-GB";
 import { MDBBtn } from "mdb-react-ui-kit";
 
-const DataVisualisation = ({sortedSessionLogs}) => {
+const DataVisualisation = ({ sortedSessionLogs }) => {
   const [fromDate, setFromDate] = useState(new Date());
   console.log("fromDate", fromDate);
   const [untilDate, setUntilDate] = useState(new Date());
@@ -12,11 +12,11 @@ const DataVisualisation = ({sortedSessionLogs}) => {
   const [dataVisForm, setDataVisForm] = useState({
     startDate: "",
     endDate: "",
-    frequency: ""
+    frequency: "",
   });
   console.log("dataVisForm", dataVisForm);
 
-console.log('sortedSessionLogs',sortedSessionLogs)
+  console.log("sortedSessionLogs", sortedSessionLogs);
 
   useEffect(() => {
     registerLocale("en-GB", enGB);
@@ -28,56 +28,54 @@ console.log('sortedSessionLogs',sortedSessionLogs)
   };
 
   // array for the drop down enabling the user to change the time segments they can see on the chart
-  const dataViewFrequency = ["Week", "Month", "Quarter", "Year"]
+  const dataViewFrequency = ["Week", "Month", "Quarter", "Year"];
 
   return (
     <div>
       <h2> Data Vis.</h2>
 
       <form onSubmit={handleFormSubmit}>
-      <div style={{border: "2px solid red" }}>
-        <h3>Start / End Date</h3>
-        <DatePicker
-          selected={fromDate}
-          onChange={(date) => {
-            setDataVisForm((prevForm) => ({
-              ...prevForm,
-              startDate: date,
-            }));
-            setFromDate(date);
-          }}
-        />
+        <div style={{ border: "2px solid red" }}>
+          <h3>Start / End Date</h3>
+          <DatePicker
+            selected={fromDate}
+            onChange={(date) => {
+              setDataVisForm((prevForm) => ({
+                ...prevForm,
+                startDate: date,
+              }));
+              setFromDate(date);
+            }}
+          />
 
-        <DatePicker
-          selected={untilDate}
-          onChange={(date) => {
-            setDataVisForm((prevForm) => ({
-              ...prevForm,
-              endDate: date,
-            }));
-            setUntilDate(date);
-          }}
-        />
+          <DatePicker
+            selected={untilDate}
+            onChange={(date) => {
+              setDataVisForm((prevForm) => ({
+                ...prevForm,
+                endDate: date,
+              }));
+              setUntilDate(date);
+            }}
+          />
         </div>
 
-        <div style={{border: "2px solid blue"}}>
-        <h3>Frequency</h3>
-        <select onChange={(e) => {
-            setDataVisForm((prevForm) => ({
+        <div style={{ border: "2px solid blue" }}>
+          <h3>Frequency</h3>
+          <select
+            onChange={(e) => {
+              setDataVisForm((prevForm) => ({
                 ...prevForm,
-                frequency: e.target.value
-            }))
-        }}>
-          {dataViewFrequency.map((freq) => (
-            <option
-            value={freq}
-            key={`key:${freq}`}
-            >
+                frequency: e.target.value,
+              }));
+            }}
+          >
+            {dataViewFrequency.map((freq) => (
+              <option value={freq} key={`key:${freq}`}>
                 {freq}
-            </option>
-          ))}
-        </select>
-        
+              </option>
+            ))}
+          </select>
         </div>
 
         <MDBBtn type="submit">Submit!</MDBBtn>
