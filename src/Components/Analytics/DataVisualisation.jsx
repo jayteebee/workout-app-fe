be74b3039fd3d15b8fd10ca4026f467455a5be84
Jethro_Block <jethroblock22@gmail.COM>
2023-12-04 13:17:52 +0000

    create dateFrom, dateUntil state management

--- src/Components/Analytics/DataVisualisation.jsx
+++ src/Components/Analytics/DataVisualisation.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DatePicker, { registerLocale } from "react-datepicker";
+import enGB from "date-fns/locale/en-GB";
+import { MDBBtn } from 'mdb-react-ui-kit';
 
 const DataVisualisation = () => {
+    const [fromDate, setFromDate] = useState(new Date());
+    console.log("fromDate", fromDate);
+    const [untilDate, setUntilDate] = useState(new Date());
+    console.log("untilDate", untilDate);
+
+    const [dataVisForm, setDataVisForm] = useState({
+        startDate: "",
+        endDate: ""
+      });
+      console.log("dataVisForm", dataVisForm);
+
+    useEffect(() => {
+        registerLocale("en-GB", enGB);
+      }, []);
+
+
+      const handleFormSubmit = (e) => {
+        e.preventDefault()
+        console.log('submitted',)
+      }
   return (
-    <div>DataVisualisation</div>
+    <div>
+    <h2> Data Vis.</h2>
+
+<form
+onSubmit={handleFormSubmit}
+>
+<DatePicker
+selected={fromDate}
+onChange={(date) => {
+    setDataVisForm((prevForm) => ({
+        ...prevForm,
+    startDate: date,
+  }));
+  setFromDate(date);
+}}
+/>
+
+<DatePicker
+selected={untilDate}
+onChange={(date) => {
+    setDataVisForm((prevForm) => ({
+        ...prevForm,
+    endDate: date,
+  }));
+  setUntilDate(date);
+}}
+/>
+
+
+
+<MDBBtn type='submit'>Submit!</MDBBtn>
+</form>
+
+    
+    </div>
   )
 }
 
