93920a95e4c0576ce2dbf9588aa1f8a55383c5fb
Jethro_Block <jethroblock22@gmail.COM>
2023-12-05 09:45:35 +0000

    create data vis form component

---
+++ src/Components/Analytics/DataVisForm.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import DatePicker, { registerLocale } from "react-datepicker";
+import enGB from "date-fns/locale/en-GB";
+import { MDBBtn } from "mdb-react-ui-kit";
+
+const DataVisForm = () => {
+  return (
+    <div>
+    <h2> Data Vis.</h2>
+
+    <form onSubmit={handleFormSubmit}>
+      <div style={{ border: "2px solid red" }}>
+        <h3>Start / End Date</h3>
+        <DatePicker
+          selected={fromDate}
+          onChange={(date) => {
+            setDataVisForm((prevForm) => ({
+              ...prevForm,
+              startDate: date,
+            }));
+            setFromDate(date);
+          }}
+        />
+
+        <DatePicker
+          selected={untilDate}
+          onChange={(date) => {
+            setDataVisForm((prevForm) => ({
+              ...prevForm,
+              endDate: date,
+            }));
+            setUntilDate(date);
+          }}
+        />
+      </div>
+
+      <div style={{ border: "2px solid blue" }}>
+        <h3>Frequency</h3>
+        <select
+          onChange={(e) => {
+            setDataVisForm((prevForm) => ({
+              ...prevForm,
+              frequency: e.target.value,
+            }));
+          }}
+        >
+          {dataViewFrequency.map((freq) => (
+            <option value={freq} key={`key:${freq}`}>
+              {freq}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div style={{ border: "2px solid green" }}>
+        <h3> Workout / Exercise To Measure</h3>
+
+        <select
+          onChange={(e) => {
+            setDataVisForm((prevForm) => ({
+              ...prevForm,
+              workoutToMeasure: e.target.value,
+            }));
+          }}
+        >
+          <option>---Select---</option>
+          {workoutNamesArrayWithNoDuplicates.map((workout) => (
+            <option value={workout} key={`key:${workout}`}>
+              {workout}
+            </option>
+          ))}
+        </select>
+
+        <select
+          onChange={(e) => {
+            setDataVisForm((prevForm) => ({
+              ...prevForm,
+              exerciseToMeasure: e.target.value,
+            }));
+          }}
+        >
+          <option>---Select---</option>
+          {exerciseNameArrayWithNoDuplicates.map((exercise) => (
+            <option value={exercise} key={`key:${exercise}`}>
+              {exercise}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div style={{ border: "2px solid black" }}>
+        <h3>Metric To Measure</h3>
+        <select
+          onChange={(e) => {
+            setDataVisForm((prevForm) => ({
+              ...prevForm,
+              metric: e.target.value,
+            }));
+          }}
+        >
+          <option>---Select---</option>
+          {metricToMeasure.map((metric) => (
+            <option value={metric} key={`key:${metric}`}>
+              {metric}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <MDBBtn type="submit">Submit!</MDBBtn>
+    </form>
+    
+    </div>
+  )
+}
+
+export default DataVisForm
--- src/Components/Analytics/DataVisualisation.jsx
+++ src/Components/Analytics/DataVisualisation.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import DatePicker, { registerLocale } from "react-datepicker";
 import enGB from "date-fns/locale/en-GB";
 import { MDBBtn } from "mdb-react-ui-kit";
+import DataVisForm from "./DataVisForm";
 
 const DataVisualisation = ({ sortedSessionLogs }) => {
   const [fromDate, setFromDate] = useState(new Date());
@@ -71,111 +72,115 @@ const DataVisualisation = ({ sortedSessionLogs }) => {
 
   return (
     <div>
-      <h2> Data Vis.</h2>
-
-      <form onSubmit={handleFormSubmit}>
-        <div style={{ border: "2px solid red" }}>
-          <h3>Start / End Date</h3>
-          <DatePicker
-            selected={fromDate}
-            onChange={(date) => {
-              setDataVisForm((prevForm) => ({
-                ...prevForm,
-                startDate: date,
-              }));
-              setFromDate(date);
-            }}
-          />
-
-          <DatePicker
-            selected={untilDate}
-            onChange={(date) => {
-              setDataVisForm((prevForm) => ({
-                ...prevForm,
-                endDate: date,
-              }));
-              setUntilDate(date);
-            }}
-          />
-        </div>
-
-        <div style={{ border: "2px solid blue" }}>
-          <h3>Frequency</h3>
-          <select
-            onChange={(e) => {
-              setDataVisForm((prevForm) => ({
-                ...prevForm,
-                frequency: e.target.value,
-              }));
-            }}
-          >
-            {dataViewFrequency.map((freq) => (
-              <option value={freq} key={`key:${freq}`}>
-                {freq}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div style={{ border: "2px solid green" }}>
-          <h3> Workout / Exercise To Measure</h3>
-
-          <select
-            onChange={(e) => {
-              setDataVisForm((prevForm) => ({
-                ...prevForm,
-                workoutToMeasure: e.target.value,
-              }));
-            }}
-          >
-            <option>---Select---</option>
-            {workoutNamesArrayWithNoDuplicates.map((workout) => (
-              <option value={workout} key={`key:${workout}`}>
-                {workout}
-              </option>
-            ))}
-          </select>
-
-          <select
-            onChange={(e) => {
-              setDataVisForm((prevForm) => ({
-                ...prevForm,
-                exerciseToMeasure: e.target.value,
-              }));
-            }}
-          >
-            <option>---Select---</option>
-            {exerciseNameArrayWithNoDuplicates.map((exercise) => (
-              <option value={exercise} key={`key:${exercise}`}>
-                {exercise}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div style={{ border: "2px solid black" }}>
-          <h3>Metric To Measure</h3>
-          <select
-            onChange={(e) => {
-              setDataVisForm((prevForm) => ({
-                ...prevForm,
-                metric: e.target.value,
-              }));
-            }}
-          >
-            <option>---Select---</option>
-            {metricToMeasure.map((metric) => (
-              <option value={metric} key={`key:${metric}`}>
-                {metric}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <MDBBtn type="submit">Submit!</MDBBtn>
-      </form>
+        <DataVisForm
+        
+        />
     </div>
-  );
+    );
 };
 
 export default DataVisualisation;
+
+// <h2> Data Vis.</h2>
+
+// <form onSubmit={handleFormSubmit}>
+//   <div style={{ border: "2px solid red" }}>
+//     <h3>Start / End Date</h3>
+//     <DatePicker
+//       selected={fromDate}
+//       onChange={(date) => {
+//         setDataVisForm((prevForm) => ({
+//           ...prevForm,
+//           startDate: date,
+//         }));
+//         setFromDate(date);
+//       }}
+//     />
+
+//     <DatePicker
+//       selected={untilDate}
+//       onChange={(date) => {
+//         setDataVisForm((prevForm) => ({
+//           ...prevForm,
+//           endDate: date,
+//         }));
+//         setUntilDate(date);
+//       }}
+//     />
+//   </div>
+
+//   <div style={{ border: "2px solid blue" }}>
+//     <h3>Frequency</h3>
+//     <select
+//       onChange={(e) => {
+//         setDataVisForm((prevForm) => ({
+//           ...prevForm,
+//           frequency: e.target.value,
+//         }));
+//       }}
+//     >
+//       {dataViewFrequency.map((freq) => (
+//         <option value={freq} key={`key:${freq}`}>
+//           {freq}
+//         </option>
+//       ))}
+//     </select>
+//   </div>
+
+//   <div style={{ border: "2px solid green" }}>
+//     <h3> Workout / Exercise To Measure</h3>
+
+//     <select
+//       onChange={(e) => {
+//         setDataVisForm((prevForm) => ({
+//           ...prevForm,
+//           workoutToMeasure: e.target.value,
+//         }));
+//       }}
+//     >
+//       <option>---Select---</option>
+//       {workoutNamesArrayWithNoDuplicates.map((workout) => (
+//         <option value={workout} key={`key:${workout}`}>
+//           {workout}
+//         </option>
+//       ))}
+//     </select>
+
+//     <select
+//       onChange={(e) => {
+//         setDataVisForm((prevForm) => ({
+//           ...prevForm,
+//           exerciseToMeasure: e.target.value,
+//         }));
+//       }}
+//     >
+//       <option>---Select---</option>
+//       {exerciseNameArrayWithNoDuplicates.map((exercise) => (
+//         <option value={exercise} key={`key:${exercise}`}>
+//           {exercise}
+//         </option>
+//       ))}
+//     </select>
+//   </div>
+
+//   <div style={{ border: "2px solid black" }}>
+//     <h3>Metric To Measure</h3>
+//     <select
+//       onChange={(e) => {
+//         setDataVisForm((prevForm) => ({
+//           ...prevForm,
+//           metric: e.target.value,
+//         }));
+//       }}
+//     >
+//       <option>---Select---</option>
+//       {metricToMeasure.map((metric) => (
+//         <option value={metric} key={`key:${metric}`}>
+//           {metric}
+//         </option>
+//       ))}
+//     </select>
+//   </div>
+
+//   <MDBBtn type="submit">Submit!</MDBBtn>
+// </form>
