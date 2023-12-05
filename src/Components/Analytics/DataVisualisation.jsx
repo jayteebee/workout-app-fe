66f9297e119f6655ec19aab6b3b89efb3d97abd0
Jethro_Block <jethroblock22@gmail.COM>
2023-12-05 11:08:26 +0000

    formatting

--- src/Components/Analytics/DataVisualisation.jsx
+++ src/Components/Analytics/DataVisualisation.jsx
@@ -14,28 +14,31 @@ const DataVisualisation = ({ sortedSessionLogs }) => {
     metric: "",
   });
   console.log("dataVisForm", dataVisForm);
-const [datesSegmentedByChosenFrequency, setDatesSegmentedByChosenFrequency] = useState([])
-console.log('datesSegmentedByChosenFrequency',datesSegmentedByChosenFrequency)
+  const [datesSegmentedByChosenFrequency, setDatesSegmentedByChosenFrequency] =
+    useState([]);
+  console.log(
+    "datesSegmentedByChosenFrequency",
+    datesSegmentedByChosenFrequency
+  );
 
-useEffect(() => {
+  useEffect(() => {
     if (dataVisForm.startDate && dataVisForm.endDate && dataVisForm.frequency) {
-        const millisecondsInDay = 24 * 60 * 60 * 1000;
-        const millisecondsInWeek = 7 * millisecondsInDay;
+      const millisecondsInDay = 24 * 60 * 60 * 1000;
+      const millisecondsInWeek = 7 * millisecondsInDay;
 
-        const startDate = dataVisForm.startDate.getTime()
-        const endDate = dataVisForm.endDate.getTime()
-        const freq = dataVisForm.frequency
-        console.log('',startDate, endDate)
+      const startDate = dataVisForm.startDate.getTime();
+      const endDate = dataVisForm.endDate.getTime();
+      const freq = dataVisForm.frequency;
 
-        let arrayOfDates = []
-        if (freq === "Week") {
-            for (let i = startDate; i <= endDate; i += millisecondsInWeek) {
-                arrayOfDates.push(new Date(i))
-            }
+      let arrayOfDates = [];
+      if (freq === "Week") {
+        for (let i = startDate; i <= endDate; i += millisecondsInWeek) {
+          arrayOfDates.push(new Date(i));
         }
-        setDatesSegmentedByChosenFrequency(arrayOfDates)
+      }
+      setDatesSegmentedByChosenFrequency(arrayOfDates);
     }
-}, [dataVisForm])
+  }, [dataVisForm]);
 
   return (
     <div>
