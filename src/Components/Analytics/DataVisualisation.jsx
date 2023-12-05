b36e2e4f2f237a8e539691ce309b9ed3591ce859
Jethro_Block <jethroblock22@gmail.COM>
2023-12-05 10:59:45 +0000

    add time period counter

--- src/Components/Analytics/DataVisualisation.jsx
+++ src/Components/Analytics/DataVisualisation.jsx
@@ -17,9 +17,21 @@ const DataVisualisation = ({ sortedSessionLogs }) => {
 
 useEffect(() => {
     if (dataVisForm.startDate && dataVisForm.endDate && dataVisForm.frequency) {
+        const millisecondsInDay = 24 * 60 * 60 * 1000;
+        const millisecondsInWeek = 7 * millisecondsInDay;
+
         const startDate = dataVisForm.startDate.getTime()
         const endDate = dataVisForm.endDate.getTime()
-console.log('startDate',startDate, "endDate",endDate )
+        const freq = dataVisForm.frequency
+        console.log('',startDate, endDate)
+
+        if (freq === "Week") {
+            for (let i = startDate; i <= endDate; i += millisecondsInWeek) {
+                console.log('yes!', new Date(i))
+            }
+        }
+
+// console.log('startDate',startDate, "endDate",endDate )
 
     }
 }, [dataVisForm])
