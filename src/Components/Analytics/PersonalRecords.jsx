abc8fbd5a6f6d26c07db999d598c36708d68b9a6
Jethro_Block <jethroblock22@gmail.COM>
2023-12-13 10:37:09 +0000

    add table rendering of data

--- src/Components/Analytics/PersonalRecords.jsx
+++ src/Components/Analytics/PersonalRecords.jsx
@@ -1,50 +1,168 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const PersonalRecords = ({ sortedSessionLogs }) => {
-    const [arrayOfExerciseObjects, setArrayOfExerciseObjects] = useState([]);
-    console.log('arrayOfExerciseObjects',arrayOfExerciseObjects)
-    useEffect(() => {
-        let exerciseIds = new Set();
+  console.log("sortedSessionLogs", sortedSessionLogs);
+  const [arrayOfExerciseObjects, setArrayOfExerciseObjects] = useState([]);
+  console.log("arrayOfExerciseObjects", arrayOfExerciseObjects);
+  useEffect(() => {
+    let exerciseIds = new Set();
 
-        sortedSessionLogs && sortedSessionLogs.length > 0 && 
-        sortedSessionLogs.forEach((log) => {
-            const exerciseSession = log.details.exercise_sessions;
+    sortedSessionLogs &&
+      sortedSessionLogs.length > 0 &&
+      sortedSessionLogs.forEach((log) => {
+        const exerciseSession = log.details.exercise_sessions;
 
-            exerciseSession.forEach((exercise) => {
-                exerciseIds.add(exercise.exercise_id);
-            });
+        exerciseSession.forEach((exercise) => {
+          exerciseIds.add(exercise.exercise_id);
         });
+      });
 
-        const newExerciseObjects = [];
+    const newExerciseObjects = [];
 
-        exerciseIds.forEach((id) => {
-            const matchingExercise = sortedSessionLogs.find((log) =>
-                log.details.exercise_sessions.some((exercise) => exercise.exercise_id === id)
+    exerciseIds.forEach((id) => {
+      const matchingExercise = sortedSessionLogs.find((log) =>
+        log.details.exercise_sessions.some(
+          (exercise) => exercise.exercise_id === id
+        )
+      );
+
+      if (matchingExercise) {
+        const exercise = matchingExercise.details.exercise_sessions.find(
+          (ex) => ex.exercise_id === id
+        );
+
+        const newExerciseObject = {
+          exerciseName: exercise.exercise_name,
+          exerciseId: exercise.exercise_id,
+          oneRepMax: 0,
+          highestVolumeSingleSet: 0,
+          strongestTimeUnderTension: 0,
+          estimatedOneRepMax: 0,
+        };
+
+        newExerciseObjects.push(newExerciseObject);
+      }
+    });
+
+    // Update the state after the loop finishes
+    setArrayOfExerciseObjects((prevState) => [
+      ...prevState,
+      ...newExerciseObjects,
+    ]);
+  }, [sortedSessionLogs]);
+
+  useEffect(() => {
+    if (arrayOfExerciseObjects && arrayOfExerciseObjects.length > 0) {
+      let exerciseIds = new Set();
+
+      sortedSessionLogs &&
+        sortedSessionLogs.length > 0 &&
+        sortedSessionLogs.forEach((log) => {
+          const exerciseSession = log.details.exercise_sessions;
+
+          exerciseSession.forEach((exercise) => {
+            exerciseIds.add(exercise.exercise_id);
+          });
+        });
+      exerciseIds.forEach((id) => {
+        const matchingExercise = sortedSessionLogs.filter((log) =>
+          log.details.exercise_sessions.some(
+            (exercise) => exercise.exercise_id === id
+          )
+        );
+
+        console.log("matchingExercise", matchingExercise);
+
+        matchingExercise.forEach((exObj) => {
+          exObj.details.exercise_sessions.forEach((exercise) => {
+            const reps = exercise.reps_completed;
+            const weight = exercise.weight_used;
+            const exerciseID = exercise.exercise_id;
+            const timeUnderTension = exercise.set_timer;
+            const estimated1RM = reps * 1.1307 + 0.6998;
+
+            const currentExerciseObject = arrayOfExerciseObjects.find(
+              (item) => item.exerciseId === exerciseID
             );
 
-            if (matchingExercise) {
-                const exercise = matchingExercise.details.exercise_sessions.find(
-                    (ex) => ex.exercise_id === id
+            if (currentExerciseObject) {
+              const volume = reps * weight;
+              const strongestTUT = timeUnderTension * weight;
+
+              if (volume > currentExerciseObject.highestVolumeSingleSet) {
+                const updatedArrayOfExercises = arrayOfExerciseObjects.map(
+                  (item) =>
+                    item.exerciseId === exerciseID
+                      ? { ...item, highestVolumeSingleSet: volume }
+                      : item
                 );
 
-                const newExerciseObject = {
-                    exerciseName: exercise.exercise_name,
-                    exerciseId: exercise.exercise_id,
-                    oneRepMax: 0,
-                    highestVolumeSingleSet: 0,
-                    strongestTimeUnderTension: 0,
-                    estimatedOneRepMax: 0,
-                };
+                setArrayOfExerciseObjects(updatedArrayOfExercises);
+              }
+              if (
+                strongestTUT > currentExerciseObject.strongestTimeUnderTension
+              ) {
+                const updatedArrayOfExercises = arrayOfExerciseObjects.map(
+                  (item) =>
+                    item.exerciseId === exerciseID
+                      ? { ...item, strongestTimeUnderTension: strongestTUT }
+                      : item
+                );
 
-                newExerciseObjects.push(newExerciseObject);
+                setArrayOfExerciseObjects(updatedArrayOfExercises);
+              }
+
+              if (exerciseID === 1 || exerciseID === 14 || exerciseID === 15) {
+                if (estimated1RM > currentExerciseObject.estimatedOneRepMax) {
+ 
+                      const updatedArrayOfExercises = arrayOfExerciseObjects.map(
+                        (item) =>
+                          item.exerciseId === exerciseID
+                            ? { ...item, estimatedOneRepMax: estimated1RM }
+                            : item
+                      );
+                      setArrayOfExerciseObjects(updatedArrayOfExercises);
+                }
+              }
+
             }
+          });
         });
+      });
+    }
+  }, [arrayOfExerciseObjects]);
 
-        // Update the state after the loop finishes
-        setArrayOfExerciseObjects((prevState) => [...prevState, ...newExerciseObjects]);
-    }, [sortedSessionLogs]);
-
-    return <div>PersonalRecords</div>;
+  return (
+      <div className="tableContainer individualLog">
+      <div className="tableWrapper">
+      <table className="customTable">
+      <thead>
+      <tr>
+      <th>Exercise</th>
+      <th>One Rep Max</th>
+      <th>Estimated One Rep Max</th>
+      <th>Highest Volume Single Set</th>
+      <th>Strongest Time Under Tension</th>
+      </tr>
+      </thead>
+      
+      {arrayOfExerciseObjects && arrayOfExerciseObjects.length > 0 &&
+      arrayOfExerciseObjects.map((exerciseObject) => (
+      <tbody>
+      <tr>
+                            <td>{exerciseObject.exerciseName}</td>
+                            <td>{exerciseObject.oneRepMax ? exerciseObject.oneRepMax : "N/A"}</td>
+                            <td>{exerciseObject.estimatedOneRepMax ? exerciseObject.estimatedOneRepMax : "N/A"}</td>
+                            <td>{exerciseObject.highestVolumeSingleSet}</td>
+                            <td>{exerciseObject.strongestTimeUnderTension}</td>
+                            </tr>
+                            </tbody>
+                            ))
+                        }
+                </table>
+            </div>
+      </div>
+  ) 
 };
 
 export default PersonalRecords;
