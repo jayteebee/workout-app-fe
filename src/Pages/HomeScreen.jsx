b62f97079033731b08e0736b6e713ac5d5d8944b
Jethro_Block <jethroblock22@gmail.COM>
2023-12-14 16:06:00 +0000

    event colors

--- src/Pages/HomeScreen.jsx
+++ src/Pages/HomeScreen.jsx
@@ -49,6 +49,7 @@ const HomeScreen = ({ loggedIn }) => {
       return {
         title: `Start ${data.workout_name}`,
         start: formattedDate,
+        // color: '#5766b2',
         id: data.id,
         routineWorkoutId: data.routine_workout_id,
       };
