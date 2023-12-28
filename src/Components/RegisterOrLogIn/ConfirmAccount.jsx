fa2137edcb896ccffe44df521f01a582019e0c01
Jethro_Block <jethroblock22@gmail.COM>
2023-12-28 19:27:18 +0000

    Refactor axiosInstance.js and ConfirmAccount.jsx

--- src/Components/RegisterOrLogIn/ConfirmAccount.jsx
+++ src/Components/RegisterOrLogIn/ConfirmAccount.jsx
@@ -1,29 +1,13 @@
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import axiosInstance from '../../API/AxiosInstances/axiosInstance';
 
 const ConfirmAccount = () => {
   const location = useLocation();
     const nav = useNavigate()
   useEffect(() => {
-    // console.log(window.location.href); // Full URL
-    // console.log(window.location.search); // Just the query string part
-    
-    //     const tokenTest = new URLSearchParams(window.location.search).get('confirmation_token');
-    //     console.log(tokenTest); // Should log the token or null
-    
-    
-    //     const queryParams = new URLSearchParams(location.search);
-    //     console.log('queryParams',queryParams)
-    
-    //     const token = queryParams.get('confirmation_token');
-    // console.log('token',token)
-    
     const segmentedUrl = window.location.search
-console.log('segmentedUrl',segmentedUrl)
     const confirmationToken = segmentedUrl.split("=")[1]
-    console.log('confirmationToken',confirmationToken)
 
     if (confirmationToken) {
       axiosInstance.post('/confirm_account', { confirmation_token: confirmationToken })
@@ -37,8 +21,8 @@ console.log('segmentedUrl',segmentedUrl)
   }, [location]);
 
   return (
-    <div>
-    <h2>Confirmation Page</h2>
+    <div>  
+    <p style={{color: "white"}}>Redirecting...</p>
     </div>
   );
 };
