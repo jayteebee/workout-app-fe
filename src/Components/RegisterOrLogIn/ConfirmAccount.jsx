48d7d0b0e528a067c566c2ab729bd597a33f9a2f
Jethro_Block <jethroblock22@gmail.COM>
2023-12-28 16:58:14 +0000

    Add ConfirmAccount component for email confirmation

---
+++ src/Components/RegisterOrLogIn/ConfirmAccount.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const ConfirmAccount = () => {
+  const location = useLocation();
+    const nav = useNavigate()
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const token = queryParams.get('confirmation_token');
+
+    if (token) {
+      axios.post('/api/confirmation', { confirmation_token: token })
+        .then(response => {
+            nav("/GettingStarted")
+        })
+        .catch(error => {
+            console.log('Error:',error)
+        });
+    }
+  }, [location]);
+
+  return (
+    <div>
+    <h2>Confirmation Page</h2>
+    </div>
+  );
+};
+
+export default ConfirmAccount;
