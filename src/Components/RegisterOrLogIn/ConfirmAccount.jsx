7c6f96d5af3d2e00d7fd7790388060e431b35b8d
Jethro_Block <jethroblock22@gmail.COM>
2023-12-28 18:05:17 +0000

    Refactor ConfirmAccount component to remove console logs and update token retrieval

--- src/Components/RegisterOrLogIn/ConfirmAccount.jsx
+++ src/Components/RegisterOrLogIn/ConfirmAccount.jsx
@@ -6,19 +6,26 @@ const ConfirmAccount = () => {
   const location = useLocation();
     const nav = useNavigate()
   useEffect(() => {
-    console.log(window.location.href); // Full URL
-    console.log(window.location.search); // Just the query string part
+    // console.log(window.location.href); // Full URL
+    // console.log(window.location.search); // Just the query string part
     
-    const tokenTest = new URLSearchParams(window.location.search).get('confirmation_token');
-    console.log(tokenTest); // Should log the token or null
+    //     const tokenTest = new URLSearchParams(window.location.search).get('confirmation_token');
+    //     console.log(tokenTest); // Should log the token or null
     
+    
+    //     const queryParams = new URLSearchParams(location.search);
+    //     console.log('queryParams',queryParams)
+    
+    //     const token = queryParams.get('confirmation_token');
+    // console.log('token',token)
+    
+    const segmentedUrl = window.location.search
+console.log('segmentedUrl',segmentedUrl)
+    const confirmationToken = segmentedUrl.split("=")[1]
+    console.log('confirmationToken',confirmationToken)
 
-    const queryParams = new URLSearchParams(location.search);
-    console.log('queryParams',queryParams)
-    const token = queryParams.get('confirmation_token');
-console.log('token',token)
-    if (token) {
-      axios.post('/confirm_account', { confirmation_token: token })
+    if (confirmationToken) {
+      axios.post('/confirm_account', { confirmation_token: confirmationToken })
         .then(response => {
             nav("/GettingStarted")
         })
